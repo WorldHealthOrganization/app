647b1304240da1a239955d3f118311c68d5bdea2
Advay Mengle <source@madvay.com>
2020-03-18 13:48:12 -0700

    Scaffolding

---
+++ covidapp/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import HomeScreen from './components/HomeScreen'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
