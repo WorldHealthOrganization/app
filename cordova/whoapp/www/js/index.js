d9c2eed1e4dfb3ec1123617ed7ebc885610bb86a
Mark Graves <markgraves@MacBook-Pro.local>
2020-03-18 20:58:58 -0700

    js error

--- cordova/whoapp/www/js/index.js
+++ cordova/whoapp/www/js/index.js
@@ -33,12 +33,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
     },
 
