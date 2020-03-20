693e197f05f6230ec5609dfe16bbb23ec68c9399
Mark Graves <mark@myire.com>
2020-03-20 04:49:49 +0000

    normalize layout

--- client/ionic/src/App.tsx
+++ client/ionic/src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-} from '@ionic/react';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle, camera, medkit } from 'ionicons/icons';
 
 import About from './pages/About';
 import Menu from './pages/Menu';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
 import Triage from './pages/Triage';
-
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,7 +30,6 @@ import './theme/variables.css';
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
         <IonRouterOutlet>
           <Route path="/about" component={About} exact={true} />
           <Route path="/menu" component={Menu} exact={true} />
@@ -50,24 +38,8 @@ const App: React.FC = () => (
           <Route path="/triage" component={Triage} />
           <Route path="/" render={() => <Redirect to="/about" />} exact={true} />
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={camera} />
-            <IonLabel>Camera</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/triage">
-            <IonIcon icon={medkit} />
-            <IonLabel>Triage</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
 
 export default App;
-
--- client/ionic/src/pages/Triage.tsx
+++ client/ionic/src/pages/Triage.tsx
@@ -1,48 +1,45 @@
 import React from 'react';
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/react';
-import * as Survey from 'survey-react';
-import 'survey-react/survey.css';
-import styles from './Triage.module.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem } from '@ionic/react';
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
 import triage from '../surveys/triage.json';
 
-interface TriageState {
-  survey: Survey.ReactSurveyModel;
+type TriageState = {
+    survey: Survey.ReactSurveyModel
 }
 
 class Triage extends React.Component<{}, TriageState> {
-  constructor(args: Readonly<{}>) {
-    super(args);
-    const survey = new Survey.ReactSurveyModel(triage);
-    survey.onComplete.add(survey => console.log(survey.data));
-    this.state = {
-      survey,
-    };
-  }
+    constructor(args: any) {
+        super(args);
+        let survey = new Survey.ReactSurveyModel(triage);
+        survey.onComplete.add((survey) => console.log(survey.data));
+        this.state = {
+            survey: survey
+        };
+
+    }
 
   render() {
-    return (
-      <IonPage className={styles.page}>
-        <IonHeader>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            <IonItem href="/menu" color="primary">WHO</IonItem>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle>Tab 3</IonTitle>
+            <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Tab 3</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <Survey.Survey model={this.state.survey} />
-        </IonContent>
-      </IonPage>
-    );
-  }
+        <Survey.Survey model={this.state.survey} />
+      </IonContent>
+    </IonPage>
+  );
+};
 }
 export default Triage;
+
