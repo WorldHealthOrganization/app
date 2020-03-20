23a8629c0e9369b7caf23086066405f212d06f07
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-03-19 22:07:52 -0700

    Build failed I'm not sure why

--- client/ionic/src/components/ShareButton.tsx
+++ client/ionic/src/components/ShareButton.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { IonContent, IonButton } from "@ionic/react";
-import { SocialSharing } from "@ionic-native/social-sharing";
+import { SocialSharing } from "@ionic-native/social-sharing/";
 
 interface ShareConfig {
   shareStr: string;
