cc959d2d96a6e05652860a105716e93e1537b7ab
Mark Graves <mark@myire.com>
2020-03-20 02:21:36 +0000

    replace html with Ionic react components

--- client/ionic/src/pages/About.tsx
+++ client/ionic/src/pages/About.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonList, IonItem, IonLabel } from '@ionic/react';
 import styles from './About.module.css';
 
 const About: React.FC = () => {
   return (
     <IonPage className={styles.page}>
       <IonContent>
-        <div className="container">
-          <ul>
-            <li>Get the latest information</li>
-            <li>Learn how to protect yourself</li>
-            <li>Report sickness</li>
-          </ul>
-        </div>
+        <IonList>
+          <IonItem>
+            <IonLabel>Get the latest information</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Learn how to protect yourself</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Report sickness</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
