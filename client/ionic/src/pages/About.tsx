ea40548a7fb421d69dd7c6963811e7b4b3e1c5d1
Mark Graves <mark@myire.com>
2020-03-20 02:43:56 +0000

    sliding cards for about content instead of paragraphs

--- client/ionic/src/pages/About.tsx
+++ client/ionic/src/pages/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonPage, IonSlides, IonSlide } from '@ionic/react';
+import { IonContent, IonPage, IonSlides, IonSlide, IonCard, IonCardContent } from '@ionic/react';
 import styles from './About.module.css';
 
 const About: React.FC = () => {
@@ -8,13 +8,25 @@ const About: React.FC = () => {
       <IonContent>
         <IonSlides pager={true}>
           <IonSlide>
-           <p>Get the latest information</p>
+            <IonCard>
+              <IonCardContent>
+               <p>Get the latest information</p>
+              </IonCardContent>
+            </IonCard>
           </IonSlide>
           <IonSlide>
-           <p>Learn how to protect yourself</p>
+            <IonCard>
+              <IonCardContent>
+               <p>Learn how to protect yourself</p>
+              </IonCardContent>
+            </IonCard>
           </IonSlide>
           <IonSlide>
-           <p>Report sickness</p>
+            <IonCard>
+              <IonCardContent>
+               <p>Report sickness</p>
+              </IonCardContent>
+            </IonCard>
           </IonSlide>
         </IonSlides>
       </IonContent>
