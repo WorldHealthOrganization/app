415c3316c811220d5a514a1666d12b0852a63190
Mark Graves <mark@myire.com>
2020-03-21 01:55:33 +0000

    fixes from rebase

--- client/ionic/src/pages/Triage.tsx
+++ client/ionic/src/pages/Triage.tsx
@@ -1,45 +1,37 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem } from '@ionic/react';
-import * as Survey from "survey-react";
-import "survey-react/survey.css";
+import * as Survey from 'survey-react';
+import 'survey-react/survey.css';
 import triage from '../surveys/triage.json';
 
-type TriageState = {
-    survey: Survey.ReactSurveyModel
+interface TriageState {
+  survey: Survey.ReactSurveyModel;
 }
 
 class Triage extends React.Component<{}, TriageState> {
-    constructor(args: any) {
-        super(args);
-        let survey = new Survey.ReactSurveyModel(triage);
-        survey.onComplete.add((survey) => console.log(survey.data));
-        this.state = {
-            survey: survey
-        };
-
-    }
+  constructor(args: Readonly<{}>) {
+    super(args);
+    const survey = new Survey.ReactSurveyModel(triage);
+    survey.onComplete.add(survey => console.log(survey.data));
+    this.state = {
+      survey,
+    };
+  }
 
   render() {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>
-            <IonItem href="/menu" color="primary">WHO</IonItem>
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonItem href="/menu" color="primary">WHO</IonItem>
           </IonToolbar>
         </IonHeader>
-        <Survey.Survey model={this.state.survey} />
-      </IonContent>
-    </IonPage>
-  );
-};
+        <IonContent>
+          <Survey.Survey model={this.state.survey} />
+        </IonContent>
+      </IonPage>
+    );
+  }
 }
 export default Triage;
 
