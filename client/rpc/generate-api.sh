f2c00be2a42ed9e3d40fb2145cd5588098cb2dbb
Bob Lee <bob@present.co>
2020-03-21 11:10:04 -0700

    Add license and download link

--- client/rpc/generate-api.sh
+++ client/rpc/generate-api.sh
@@ -3,5 +3,6 @@
 
 cd $(dirname "$0")/..
 
+# Downloaded v0.1 from here: https://github.com/presentco/present-rpc/tree/master/java/compiler
 # TODO: Update output directory
 java -jar lib/present-rpc-compiler.jar --proto_path=../proto --js_out=react/src
