dde71dbcf23e4a58219b7bb2d08dd3d489f44ebc
Mark Graves <mark@myire.com>
2020-03-21 22:51:47 +0000

    initial content with material from WHO website

--- client/ionic/src/pages/ProtectYourself.tsx
+++ client/ionic/src/pages/ProtectYourself.tsx
@@ -3,13 +3,13 @@ import {
   IonContent,
   IonHeader,
   IonItem,
+  IonImg,
   IonPage,
   IonToolbar,
   IonSlides,
   IonSlide,
   IonCard,
-  IonCardContent,
-  IonImg
+  IonCardContent
 } from '@ionic/react';
 import 'tachyons';
 
@@ -32,41 +32,65 @@ const ProtectYourself: React.FC = () => {
           <IonSlide>
             <IonCard>
               <IonCardContent className="pb3 tc ph5">
+                <IonImg
+                  className="w-80 center"
+                  src="assets/img/protect_yourself/wash_hands.png"
+                />
                 <b>Wash your hands</b> often with soap and running water frequently
               </IonCardContent>
             </IonCard>
           </IonSlide>
           <IonSlide>
             <IonCard>
-              <IonCardContent>
+              <IonCardContent className="pb3 tc ph5">
+                <IonImg
+                  className="w-80 center"
+                  src="assets/img/protect_yourself/cover.png"
+                />
                 <b>When coughing and squeezing cover mouth and nose</b> with flexed elbow or tissue
               </IonCardContent>
             </IonCard>
           </IonSlide>
           <IonSlide>
             <IonCard>
-              <IonCardContent>
+              <IonCardContent className="pb3 tc ph5">
+                <IonImg
+                  className="w-80 center"
+                  src="assets/img/protect_yourself/closed_bin.png"
+                />
                 Throw tissue into <b>closed bin</b> immediately after use
               </IonCardContent>
             </IonCard> 
           </IonSlide>
           <IonSlide>
             <IonCard>
-              <IonCardContent>
+              <IonCardContent className="pb3 tc ph5">              
+                <IonImg
+                  className="w-80 center"
+                  src="assets/img/protect_yourself/wash_hands_often.png"
+                />
                 Wash your hands <b>frequently</b>
               </IonCardContent>
             </IonCard> 
           </IonSlide>
           <IonSlide>
             <IonCard>
-              <IonCardContent>
+              <IonCardContent className="pb3 tc ph5">              
+                <IonImg
+                  className="w-80 center"
+                  src="assets/img/protect_yourself/avoid_close_contact.png"
+                />
                 <b>Avoid close contact</b> and keep the physical distancing                
               </IonCardContent>
             </IonCard>
           </IonSlide>
           <IonSlide>
             <IonCard>
-              <IonCardContent>
+              <IonCardContent className="pb3 tc ph5">              
+                <IonImg
+                  className="w-80 center"
+                  src="assets/img/protect_yourself/seek_medical_care.png"
+                />
                 <b>Seek medical care early</b> if you have fever, cough, and difficulty breathing
               </IonCardContent>
             </IonCard>
