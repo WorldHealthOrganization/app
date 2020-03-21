5ac35086c481035b3ee0057c83a5dbfcdcb5008d
pjayathissa <p.jayathissa@gmail.com>
2020-03-21 21:48:25 +1300

    added styling to menu

--- client/ionic/src/pages/Menu.tsx
+++ client/ionic/src/pages/Menu.tsx
@@ -7,31 +7,27 @@ import {
   IonToolbar,
   IonItem,
   IonLabel,
+  IonImg
 } from '@ionic/react';
 
 const Menu: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>
-            <IonItem href="/menu" color="primary">
-              WHO
-            </IonItem>
-          </IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    <IonPage className="pa3">
       <IonContent>
-        <IonItem href="/protect_yourself">
+      <IonImg
+          className="w-80 center pb5"
+          src="assets/identity/who-logo-rgb.png"
+        />
+        <IonItem href="/protect_yourself" color="primary" className = "pb3" >
           <IonLabel>Protect Yourself</IonLabel>
         </IonItem>
-        <IonItem href="/triage">
+        <IonItem href="/triage" color="primary" className = "pb3" >
           <IonLabel>Check Your Health</IonLabel>
         </IonItem>
-        <IonItem href="/local_maps">
+        <IonItem href="/local_maps" color="primary" className = "pb3" >
           <IonLabel>Local Maps</IonLabel>
         </IonItem>
-        <IonItem href="/share">
+        <IonItem href="/share" color="primary" className = "pb3" >
           <IonLabel>Share the App</IonLabel>
         </IonItem>
       </IonContent>
