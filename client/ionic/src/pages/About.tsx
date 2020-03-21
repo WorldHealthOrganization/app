04d144eb09716bb87d437caaab578daa4b8f2798
Mark Graves <mark@myire.com>
2020-03-21 01:06:53 +0000

    add WHO logo, copy content from static designs for about pages

--- client/ionic/src/pages/About.tsx
+++ client/ionic/src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { IonContent, IonPage, IonSlides, IonSlide, IonCard, IonCardContent, IonButton } from '@ionic/react';
+import { IonContent, IonPage, IonSlides, IonSlide, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonImg, IonIcon } from '@ionic/react';
+import { personAddOutline, handRightOutline, handLeftOutline, peopleOutline } from 'ionicons/icons';
 
 const About: React.FC = () => {
   return (
@@ -9,23 +10,65 @@ const About: React.FC = () => {
           <IonSlide>
             <IonCard>
               <IonCardContent>
-               <p>Get the latest information</p>
+               <IonImg src="assets/identity/who-logo-rgb.png" />
+               <IonCardHeader>
+                 <IonCardTitle>Official WHO App for COVID-19</IonCardTitle>
+                 <IonCardSubtitle>Learn how to protect yourself and your community.</IonCardSubtitle>
+                 <IonCardSubtitle>Find medical resources to help.</IonCardSubtitle>
+               </IonCardHeader>
                <IonButton href="/Menu" shape="round">Learn More</IonButton>
               </IonCardContent>
             </IonCard>
           </IonSlide>
           <IonSlide>
             <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>How it Spreads</IonCardTitle>
+                <IonCardSubtitle>COVID-19 also referred to as "Coronavirus" mainly spreads from person-to-person between people who are in close contact with one another (within about 6 feet or two meters)  through respiratory droplets produced when an infected person coughs or sneezes.</IonCardSubtitle>
+              </IonCardHeader>
               <IonCardContent>
-               <p>Learn how to protect yourself</p>
+               <IonIcon size="large" icon={personAddOutline}></IonIcon>
+              </IonCardContent>
+              <IonCardContent>
                <IonButton href="/Menu" shape="round">Learn More</IonButton>
               </IonCardContent>
             </IonCard>
           </IonSlide>
           <IonSlide>
             <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>Clean Your Hands</IonCardTitle>
+                <IonCardSubtitle>
+                  <b>Wash your hands often</b> with soad and water for at least 20 sconds, especially after you have been in a public place, or after blowing your nose, coughing or sneezing.
+                </IonCardSubtitle>
+                <IonCardSubtitle>
+                  <b>Avoid touching your eyes, nose, and mouth</b> with unwashed hands.
+                </IonCardSubtitle>
+              </IonCardHeader>
               <IonCardContent>
-               <p>Report sickness</p>
+               <IonIcon size="large" icon={handLeftOutline}></IonIcon>
+               <IonIcon size="large" icon={handRightOutline}></IonIcon>
+              </IonCardContent>
+              <IonCardContent>
+               <IonButton href="/Menu" shape="round">Learn More</IonButton>
+              </IonCardContent>
+            </IonCard>
+          </IonSlide>
+          <IonSlide>
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>Avoid Close Contact</IonCardTitle>
+                <IonCardSubtitle>
+                 <b>Avoid close contact</b> with people who are sick.
+                </IonCardSubtitle>
+                <IonCardSubtitle>
+                 <b>Maintain distance between yourself and other people</b> if COVID-19 is spreading in you community.  This is especially important for people who are at higher risk of getting very sick including the elderly.
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+               <IonIcon size="large" icon={peopleOutline}></IonIcon>
+              </IonCardContent>
+              <IonCardContent>
                <IonButton href="/Menu" shape="round">Learn More</IonButton>
               </IonCardContent>
             </IonCard>
@@ -37,3 +80,4 @@ const About: React.FC = () => {
 };
 
 export default About;
+
