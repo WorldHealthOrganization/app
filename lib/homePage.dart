88293472cbc8b78d1c953a943669b41fbca6c6d7
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-22 13:51:57 -0700

    Added share feature

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -6,68 +6,71 @@ import 'package:share/share.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(right: 20, left: 20, top: 50),
-      child: Column(
-        children: <Widget>[
-          Image(
-              image: AssetImage("assets/WHO.jpg"),
-              width: MediaQuery.of(context).size.width),
-          Container(
-            padding: EdgeInsets.only(top: 50),
-            height: 150,
-            width: MediaQuery.of(context).size.width,
-            child: FlatButton(
-              onPressed: () {},
-              color: Constants.primaryColor,
-              child: Text(
-                "Protect Yourself",
-                textScaleFactor: 2,
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(right: 20, left: 20, top: 50),
+        child: Column(
+          children: <Widget>[
+            Image(
+                image: AssetImage("assets/WHO.jpg"),
+                width: MediaQuery.of(context).size.width),
+            Container(
+              padding: EdgeInsets.only(top: 50),
+              height: 150,
+              width: MediaQuery.of(context).size.width,
+              child: FlatButton(
+                onPressed: () {},
+                color: Constants.primaryColor,
+                child: Text(
+                  "Protect Yourself",
+                  textScaleFactor: 2,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 30),
-            height: 130,
-            width: MediaQuery.of(context).size.width,
-            child: FlatButton(
-              onPressed: () {},
-              color: Constants.primaryColor,
-              child: Text(
-                "Check Your Health",
-                textScaleFactor: 2,
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              height: 130,
+              width: MediaQuery.of(context).size.width,
+              child: FlatButton(
+                onPressed: () {},
+                color: Constants.primaryColor,
+                child: Text(
+                  "Check Your Health",
+                  textScaleFactor: 2,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 30),
-            height: 130,
-            width: MediaQuery.of(context).size.width,
-            child: FlatButton(
-              onPressed: () {},
-              color: Constants.primaryColor,
-              child: Text(
-                "Local Maps",
-                textScaleFactor: 2,
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              height: 130,
+              width: MediaQuery.of(context).size.width,
+              child: FlatButton(
+                onPressed: () {},
+                color: Constants.primaryColor,
+                child: Text(
+                  "Local Maps",
+                  textScaleFactor: 2,
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 30),
-            height: 130,
-            width: MediaQuery.of(context).size.width,
-            child: FlatButton(
-              onPressed: () {
-                Share.share('Check out the official COVID-19 GUIDE App https://preview.whoapp.org/menu');
-              },
-              color: Constants.primaryColor,
-              child: Text(
-                "Share the App",
-                textScaleFactor: 2,
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              height: 130,
+              width: MediaQuery.of(context).size.width,
+              child: FlatButton(
+                onPressed: () {
+                  Share.share(
+                      'Check out the official COVID-19 GUIDE APP https://preview.whoapp.org/menu');
+                },
+                color: Constants.primaryColor,
+                child: Text(
+                  "Share the App",
+                  textScaleFactor: 2,
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
