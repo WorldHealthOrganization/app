d82fdd358e34d2d5f350d7f51e33f633868032fc
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-03-22 16:38:32 -0700

    Refactored Carousel pages

--- lib/pages/protectYourself.dart
+++ lib/pages/protectYourself.dart
@@ -1,8 +1,6 @@
-import 'package:WHOFlutter/constants.dart';
 import 'package:WHOFlutter/localization/localization.dart';
-import 'package:WHOFlutter/pageScaffold.dart';
 import 'package:flutter/material.dart';
-import 'package:page_view_indicator/page_view_indicator.dart';
+import 'package:WHOFlutter/carouselPage.dart';
 
 class ProtectYourself extends StatelessWidget {
   @override
@@ -25,82 +23,5 @@ class ProtectYourself extends StatelessWidget {
       CarouselSlide("assets/distance.png",
           AppLocalizations.of(context).translate("seekMedicalCare"), context),
     ]);
-  }
-}
-
-class CarouselSlide extends StatelessWidget {
-  CarouselSlide(this.imgSrc, this.message, this.context);
-  final String imgSrc;
-  final String message;
-  final BuildContext context;
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width * 9 / 10;
-
-    return Container(
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            Image(
-              image: AssetImage(imgSrc),
-              width: width,
-            ),
-            Text(
-              message,
-              textScaleFactor: 1.7,
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CarouselView extends StatelessWidget {
-  List<CarouselSlide> items = [];
-  CarouselView(this.items);
-
-  final pageIndexNotifier = ValueNotifier<int>(0);
-
-  @override
-  Widget build(BuildContext context) {
-    return PageScaffold(
-      Stack(
-        children: <Widget>[
-          PageView(
-            onPageChanged: (i)=>pageIndexNotifier.value = i,
-            children: this.items,
-          ),
-          Align(
-            alignment: FractionalOffset.bottomCenter,
-            child: Container(
-                padding: EdgeInsets.only(bottom: 20),
-                child: pageViewIndicator()),
-          )
-        ],
-      ),
-    );
-  }
-
-  PageViewIndicator pageViewIndicator() {
-    return PageViewIndicator(
-      pageIndexNotifier: pageIndexNotifier,
-      length: 6,
-      normalBuilder: (animationController, index) => Circle(
-        size: 8.0,
-        color: Colors.grey,
-      ),
-      highlightedBuilder: (animationController, index) => ScaleTransition(
-        scale: CurvedAnimation(
-          parent: animationController,
-          curve: Curves.ease,
-        ),
-        child: Circle(
-          size: 10.0,
-          color: Constants.primaryColor,
-        ),
-      ),
-    );
   }
 }
