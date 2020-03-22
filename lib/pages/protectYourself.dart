e0395f469a80b83714112e11f61e24de56cd1249
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-22 16:26:43 -0700

    Added page identifier

--- lib/pages/protectYourself.dart
+++ lib/pages/protectYourself.dart
@@ -2,44 +2,64 @@ import 'package:WHOFlutter/constants.dart';
 import 'package:WHOFlutter/localization/localization.dart';
 import 'package:WHOFlutter/pageScaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicator/page_view_indicator.dart';
 
 class ProtectYourself extends StatelessWidget {
   PageController pageController = new PageController();
+  final pageIndexNotifier = ValueNotifier<int>(0);
 
   @override
   Widget build(BuildContext context) {
     return PageScaffold(
-      PageView(
-        controller: pageController,
-        children: <Widget>[
-          slide("assets/washHands.png",
-              AppLocalizations.of(context).translate("washHands"), context, 0),
-          slide(
-              "assets/cough.png",
-              AppLocalizations.of(context).translate("cougningAndSneezing"),
-              context,
-              1),
-          slide(
-              "assets/cough.png",
-              AppLocalizations.of(context).translate("throwAwayTissue"),
-              context,
-              2),
-          slide(
-              "assets/washHands.png",
-              AppLocalizations.of(context).translate("washHandsFrequently"),
-              context,
-              3),
-          slide(
-              "assets/distance.png",
-              AppLocalizations.of(context).translate("socialDistancing"),
-              context,
-              4),
-          slide(
-              "assets/distance.png",
-              AppLocalizations.of(context).translate("seekMedicalCare"),
-              context,
-              5),
-        ],
+      Container(
+        height: MediaQuery.of(context).size.height / 2,
+        child: Stack(
+          children: <Widget>[
+            PageView(
+              controller: pageController,
+              onPageChanged: (index) => pageIndexNotifier.value = index,
+              children: <Widget>[
+                slide(
+                    "assets/washHands.png",
+                    AppLocalizations.of(context).translate("washHands"),
+                    context,
+                    0),
+                slide(
+                    "assets/cough.png",
+                    AppLocalizations.of(context)
+                        .translate("cougningAndSneezing"),
+                    context,
+                    1),
+                slide(
+                    "assets/cough.png",
+                    AppLocalizations.of(context).translate("throwAwayTissue"),
+                    context,
+                    2),
+                slide(
+                    "assets/washHands.png",
+                    AppLocalizations.of(context)
+                        .translate("washHandsFrequently"),
+                    context,
+                    3),
+                slide(
+                    "assets/distance.png",
+                    AppLocalizations.of(context).translate("socialDistancing"),
+                    context,
+                    4),
+                slide(
+                    "assets/distance.png",
+                    AppLocalizations.of(context).translate("seekMedicalCare"),
+                    context,
+                    5),
+              ],
+            ),
+            Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(padding: EdgeInsets.only(bottom: 20),
+              child: pageViewIndicator()),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -60,21 +80,42 @@ class ProtectYourself extends StatelessWidget {
               textScaleFactor: 1.7,
               textAlign: TextAlign.center,
             ),
-            Expanded(
-              child: Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: LinearProgressIndicator(
-                    backgroundColor: Colors.transparent,
-                    valueColor: new AlwaysStoppedAnimation<Color>(
-                        Constants.primaryColor),
-                    value: (index + 1) / 6,
-                  ),
-                ),
-              ),
-            )
+            // Expanded(
+            //   child: Align(
+            //     alignment: FractionalOffset.bottomCenter,
+            //     child: Padding(
+            //       padding: const EdgeInsets.all(8.0),
+            //       child: LinearProgressIndicator(
+            //         backgroundColor: Colors.transparent,
+            //         valueColor: new AlwaysStoppedAnimation<Color>(
+            //             Constants.primaryColor),
+            //         value: (index + 1) / 6,
+            //       ),
+            //     ),
+            //   ),
+            // )
           ],
+        ),
+      ),
+    );
+  }
+
+  PageViewIndicator pageViewIndicator() {
+    return PageViewIndicator(
+      pageIndexNotifier: pageIndexNotifier,
+      length: 6,
+      normalBuilder: (animationController, index) => Circle(
+        size: 8.0,
+        color: Colors.grey,
+      ),
+      highlightedBuilder: (animationController, index) => ScaleTransition(
+        scale: CurvedAnimation(
+          parent: animationController,
+          curve: Curves.ease,
+        ),
+        child: Circle(
+          size: 10.0,
+          color: Constants.primaryColor,
         ),
       ),
     );
