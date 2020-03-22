aa3c132433dd21d86affe56eb59b08d233442c2b
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-03-22 13:53:40 -0700

    added global page scaffold

---
+++ lib/pageScaffold.dart
@@ -0,0 +1,27 @@
+import 'package:WHOFlutter/homePage.dart';
+import 'package:flutter/material.dart';
+
+class PageScaffold extends StatelessWidget {
+  PageScaffold(this.body);
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding: EdgeInsets.all(15),
+            child: Column(children: <Widget>[
+              GestureDetector(
+                child: Image(
+                    image: AssetImage('assets/WHO.jpg'),
+                    width: MediaQuery.of(context).size.width),
+                    onTap: ()=>Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (c)=>HomePage()
+                      )
+                    ),
+              ),
+              this.body
+            ])));
+  }
+}
