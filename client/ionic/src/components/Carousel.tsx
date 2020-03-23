beefe1d04ea3729ad6746f361d272e969420d5b7
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-03-23 21:06:42 +0000

    refactor to TestImage componet

--- client/ionic/src/components/Carousel.tsx
+++ client/ionic/src/components/Carousel.tsx
@@ -14,6 +14,35 @@ import {
 import 'tachyons';
 import useDynamicFlow from '../hooks/useDynamicFlow';
 import TopNav from '../components/TopNav';
+import { TextImageScreen, LoadedFlow } from '../content/flow';
+
+const TextImage = ( { screen, flow }: { screen: TextImageScreen, flow: LoadedFlow}) => {
+  return (
+    <IonSlide>
+      <div className="flex flex-column h-100">
+        <div>
+          <IonCardTitle className="near-black">
+            {screen.headingText}
+          </IonCardTitle>
+          <IonCardSubtitle></IonCardSubtitle>
+        </div>
+        <div className="tl">
+          {screen.bodyTexts &&
+            screen.bodyTexts.map((txt, key) => (
+              <p key={key}>{txt}</p>
+            ))}
+          {screen.bottomImageUri && (
+            /* TODO: actual css */
+            <IonImg
+              className="center pt3 h4"
+              src={flow.imgPrefix + '/' + screen.bottomImageUri}
+            />
+          )}
+        </div>
+      </div>
+    </IonSlide>
+  );
+}
 
 const Carousel = ({ flow: flowName }: { flow: string }) => {
   // TODO: Refactor this out to separate Flow components. Use a dictionary
@@ -28,31 +57,7 @@ const Carousel = ({ flow: flowName }: { flow: string }) => {
             {flow.content.screens.map((screen, key) => {
               switch (screen.type) {
                 case 'TextImage':
-                  return (
-                    <IonSlide key={key}>
-                      <IonCard>
-                        <IonCardHeader>
-                          <IonCardTitle className="near-black">
-                            {screen.headingText}
-                          </IonCardTitle>
-                          <IonCardSubtitle></IonCardSubtitle>
-                        </IonCardHeader>
-                        <IonCardContent className="tl">
-                          {screen.bodyTexts &&
-                            screen.bodyTexts.map((txt, key) => (
-                              <p key={key}>{txt}</p>
-                            ))}
-                          {screen.bottomImageUri && (
-                            /* TODO: actual css */
-                            <IonImg
-                              className="center pt3 h4"
-                              src={flow.imgPrefix + '/' + screen.bottomImageUri}
-                            />
-                          )}
-                        </IonCardContent>
-                      </IonCard>
-                    </IonSlide>
-                  );
+                  return <TextImage key={key} screen={screen} flow={flow} />;
                 default:
                 /** TODO: Handle errors of unsupported screen types correctly. */
               }
