0df87b2de4ea10d00f09a1e9f8a6629840ff0bbb
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-03-23 18:36:33 +0000

    re-add link

--- client/ionic/src/pages/SplashTitle.tsx
+++ client/ionic/src/pages/SplashTitle.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { IonContent, IonPage, IonText, IonImg } from '@ionic/react';
+import { IonContent, IonPage, IonText, IonImg, IonCard } from '@ionic/react';
 import 'tachyons';
 
 const SplashTitle: React.FC = () => {
   return (
     <IonPage className="pa4">
       <IonContent style={{ '--overflow': 'hidden' }}>
-        <div className="center tc h-100 flex flex-row items-center">
+        <IonCard
+          routerLink="/splash-info"
+          className="center tc h-100 flex flex-row items-center"
+        >
           <div className="flex flex-column" style={{ marginBottom: 100 }}>
             <div>
               <IonImg
@@ -20,7 +23,7 @@ const SplashTitle: React.FC = () => {
               </IonText>
             </div>
           </div>
-        </div>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
