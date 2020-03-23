5015f28cc1ac95c0b83c68ca95535b604fca1556
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-03-23 20:53:36 +0000

    refactor to Carousel component

--- client/ionic/src/pages/CheckYourHealth.tsx
+++ client/ionic/src/pages/CheckYourHealth.tsx
@@ -1,56 +1,7 @@
 import React from 'react';
-import {
-  IonContent,
-  IonPage,
-  IonSlides,
-  IonSlide,
-  IonCard,
-  IonCardContent,
-  IonImg,
-} from '@ionic/react';
-import TopNav from '../components/TopNav';
+import Carousel from '../components/Carousel';
 import 'tachyons';
-import useDynamicFlow from '../hooks/useDynamicFlow';
 
-const CheckYourHealth: React.FC = () => {
-  // TODO: Refactor this out to separate Flow components. Use a dictionary
-  // of screen archetypes.
-  const flow = useDynamicFlow('check-your-health');
-  return (
-    <IonPage className="pa3">
-      <TopNav />
-      <IonContent>
-        {flow.content && flow.content.screens && (
-          <IonSlides pager={true}>
-            {flow.content.screens.map(screen => {
-              switch (screen.type) {
-                case 'TextImage':
-                  return (
-                    <IonSlide>
-                      <IonCard>
-                        <IonCardContent className="pb3 tl ph5">
-                          {screen.bottomImageUri && (
-                            /* TODO: actual css */
-                            <IonImg
-                              className="h4 center pb3"
-                              src={flow.imgPrefix + '/' + screen.bottomImageUri}
-                            />
-                          )}
-                          {screen.bodyTexts &&
-                            screen.bodyTexts.map(txt => <p>{txt}</p>)}
-                        </IonCardContent>
-                      </IonCard>
-                    </IonSlide>
-                  );
-                default:
-                /** TODO: Handle errors of unsupported screen types correctly. */
-              }
-            })}
-          </IonSlides>
-        )}
-      </IonContent>
-    </IonPage>
-  );
-};
+const CheckYourHealth = () => <Carousel flow="check-your-health" />;
 
 export default CheckYourHealth;
