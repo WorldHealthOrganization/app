a3d925d33a382e6c5e40ee093fe60ff7119685a9
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-03-24 01:19:55 -0400

    Improve carousel styling (#217)
    * refactor to Carousel component
    
    * refactor to TestImage componet
    
    * more carousel styles
    
    * fix font size
    
    * conditionally render screen.headingText
    
    * rm import tachyons

---
+++ client/ionic/src/components/Carousel.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { IonContent, IonPage, IonSlides, IonSlide, IonImg } from '@ionic/react';
+import 'tachyons';
+import useDynamicFlow from '../hooks/useDynamicFlow';
+import TopNav from '../components/TopNav';
+import { TextImageScreen, LoadedFlow } from '../content/flow';
+import RenderHTML from './RenderHTML';
+
+const TextImage = ({
+  screen,
+  flow,
+}: {
+  screen: TextImageScreen;
+  flow: LoadedFlow;
+}) => {
+  return (
+    <IonSlide>
+      <div className="flex flex-column justify-around h-100 pa4">
+        <div className="mt3 tl">
+          {screen.headingText && (
+            <div style={{ fontSize: '1.5em' }}>{screen.headingText}</div>
+          )}
+          <div style={{ fontSize: screen.headingText ? '1em' : '1.5em' }}>
+            {screen.bodyTexts &&
+              screen.bodyTexts.map((txt, key) => (
+                <RenderHTML source={txt} key={key} />
+              ))}
+          </div>
+        </div>
+        <div>
+          {screen.bottomImageUri && (
+            <IonImg
+              className="center pt3 h4"
+              src={flow.imgPrefix + '/' + screen.bottomImageUri}
+            />
+          )}
+        </div>
+      </div>
+    </IonSlide>
+  );
+};
+
+const Carousel = ({ flow: flowName }: { flow: string }) => {
+  const flow = useDynamicFlow(flowName);
+  const slidesStyles = {
+    '--bullet-background': 'var(--ion-color-primary-tint)',
+    '--bullet-background-active': 'var(--ion-color-primary)',
+  };
+  return (
+    <IonPage className="pa3">
+      <TopNav />
+      <IonContent className="center tc">
+        {flow.content && flow.content.screens && (
+          <IonSlides pager={true} className="h-100" style={slidesStyles}>
+            {flow.content.screens.map((screen, key) => {
+              switch (screen.type) {
+                case 'TextImage':
+                  return <TextImage key={key} screen={screen} flow={flow} />;
+                default:
+                  /** TODO: Handle errors of unsupported screen types correctly. */
+                  return null;
+              }
+            })}
+          </IonSlides>
+        )}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Carousel;
--- client/ionic/src/pages/ProtectYourself.tsx
+++ client/ionic/src/pages/ProtectYourself.tsx
@@ -1,59 +1,6 @@
 import React from 'react';
-import {
-  IonContent,
-  IonPage,
-  IonSlides,
-  IonSlide,
-  IonCard,
-  IonCardContent,
-  IonImg,
-} from '@ionic/react';
-import TopNav from '../components/TopNav';
-import 'tachyons';
-import useDynamicFlow from '../hooks/useDynamicFlow';
-import RenderHTML from '../components/RenderHTML';
+import Carousel from '../components/Carousel';
 
-const ProtectYourself: React.FC = () => {
-  // TODO: Refactor this out to separate Flow components. Use a dictionary
-  // of screen archetypes.
-  const flow = useDynamicFlow('protect-yourself');
-  return (
-    <IonPage className="pa3">
-      <TopNav />
-      <IonContent>
-        {flow.content && flow.content.screens && (
-          <IonSlides pager={true}>
-            {flow.content.screens.map(screen => {
-              switch (screen.type) {
-                case 'TextImage':
-                  return (
-                    <IonSlide>
-                      <IonCard>
-                        <IonCardContent className="pb3 tl ph5">
-                          {screen.bottomImageUri && (
-                            /* TODO: actual css */
-                            <IonImg
-                              className="h4 center pb3"
-                              src={flow.imgPrefix + '/' + screen.bottomImageUri}
-                            />
-                          )}
-                          {screen.bodyTexts &&
-                            screen.bodyTexts.map((txt, key) => (
-                              <RenderHTML source={txt} key={key} />
-                            ))}
-                        </IonCardContent>
-                      </IonCard>
-                    </IonSlide>
-                  );
-                default:
-                /** TODO: Handle errors of unsupported screen types correctly. */
-              }
-            })}
-          </IonSlides>
-        )}
-      </IonContent>
-    </IonPage>
-  );
-};
+const ProtectYourself = () => <Carousel flow="protect-yourself" />;
 
 export default ProtectYourself;
