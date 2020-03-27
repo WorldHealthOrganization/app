bf54111088bbd8c841c4b54122dd0d45934673e4
hahaha <khanabdullah4289951@gmail.com>
2020-03-28 00:24:06 +0500

    Added message_ur.dart

---
+++ client/flutter/lib/generated/intl/messages_ur.dart
@@ -0,0 +1,49 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ur locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ur';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "aboutTheApp" : MessageLookupByLibrary.simpleMessage("ایپ کے بارے میں"),
+    "checkYourHealth" : MessageLookupByLibrary.simpleMessage("اپنی صحت کی جانچ کریں"),
+    "childDistance" : MessageLookupByLibrary.simpleMessage("کوشش کریں اور بچوں کو ان کے والدین اور کنبہ کے قریب رکھیں اور ممکنہ حد تک بچوں اور ان کی دیکھ بھال کرنے والوں کو جدا کرنے سے گریز کریں۔ اگر علیحدگی ہوتی ہے (جیسے ہسپتال میں داخل ہونا) باقاعدگی سے رابطے کو یقینی بنائیں (جیسے فون کے ذریعے) اور دوبارہ یقین دہانی کرو۔"),
+    "childFacts" : MessageLookupByLibrary.simpleMessage("کیا ہوا ہے اس کے بارے میں حقائق مہیا کریں ، اب کیا ہورہا ہے اس کی وضاحت کریں اور انھیں اس بیماری سے متاثر ہونے کے خطرے کو کم کرنے کی معلومات ان الفاظ میں فراہم کریں کہ ان کی عمر کے لحاظ سے وہ سمجھ سکیں- اس میں یقین دہانی کے انداز میں یہ معلومات فراہم کرنا بھی شامل ہے کہ کیا ہوسکتا ہے۔ (مثال کے طور پر ایک کنبہ کے فرد یا بچہ کی طبیعت ٹھیک نہیں ہے اور اسے کچھ وقت کے لئے اسپتال جانا پڑ سکتا ہے تاکہ ڈاکٹر اس کو ٹھیک کرنے میں مدد کرسکیں)۔"),
+    "childLove" : MessageLookupByLibrary.simpleMessage("بچوں کو مشکل اوقات میں بڑوں کی محبت اور توجہ کی ضرورت ہوتی ہے۔ انھیں اضافی وقت اور توجہ دیں۔ اپنے بچوں کی بات سننے کے لئے یاد رکھیں ، برائے مہربانی بولیں اور تندرست ہو جائیں۔ اگر ممکن ہو تو، بچے کو کھیل کھیلنے اور آرام کرنے کے مواقع فراہم کریں۔"),
+    "childRoutines" : MessageLookupByLibrary.simpleMessage("زیادہ سے زیادہ باقاعدہ معمولات اور نظام الاوقات کو جاری رکھیں، یا اسکول سیکھنے کے ساتھ ساتھ محفوظ طریقے سے کھیلنے اور آرام کرنے کے لئے وقت سمیت ایک نئے ماحول میں نئے بنانے میں مدد کریں۔"),
+    "childStress" : MessageLookupByLibrary.simpleMessage("بچے دباؤ کا جواب مختلف طریقوں سے دے سکتے ہیں جیسے زیادہ پیچیدہ ، پریشان ہونا ، پیچھے ہٹنا ، ناراض ہونا یا مشتعل ہونا ، بیڈ گیٹ وغیرہ مددگار انداز میں آپ کے بچے کے رد عمل کا جواب دیں، ان کے خدشات سنیں اور انھیں اضافی محبت اور توجہ دیں۔"),
+    "cougningAndSneezing" : MessageLookupByLibrary.simpleMessage("جب کھانسی اور چھینکنے تو منہ اور ناک کو کہنی یا ٹشو سے ڈھانپیں"),
+    "feelingDistressed" : MessageLookupByLibrary.simpleMessage("?پریشان ہو رہا ہے"),
+    "healthCheck" : MessageLookupByLibrary.simpleMessage("صحت کی جانچ"),
+    "infoForEveryone" : MessageLookupByLibrary.simpleMessage("ہر ایک کے لئے معلومات"),
+    "infoForParents" : MessageLookupByLibrary.simpleMessage("والدین کے لئے معلومات"),
+    "learnMore" : MessageLookupByLibrary.simpleMessage("اورجانیے"),
+    "localMaps" : MessageLookupByLibrary.simpleMessage("مقامی نقشہ جات"),
+    "protectYourself" : MessageLookupByLibrary.simpleMessage("اپنے آپ کی حفاظت"),
+    "seekMedicalCare" : MessageLookupByLibrary.simpleMessage("بخار، کھانسی، اور سانس لینے میں دشواری ہو تو جلدی طبی دیکھ بھال کریں"),
+    "shareTheApp" : MessageLookupByLibrary.simpleMessage("ایپ شیئر کریں"),
+    "socialDistancing" : MessageLookupByLibrary.simpleMessage("قریبی رابطے سے گریز کریں اور جسمانی دوری برقرار رکھیں"),
+    "throwAwayTissue" : MessageLookupByLibrary.simpleMessage("ٹشو کو استعمال کے فورا. بعد بند ڈبے میں پھینک دیں"),
+    "travelAdvice" : MessageLookupByLibrary.simpleMessage("سفری مشورے"),
+    "washHands" : MessageLookupByLibrary.simpleMessage("اپنے ہاتھوں کو صابن اور بہتے ہوئے پانی سے دھوئیں"),
+    "washHandsFrequently" : MessageLookupByLibrary.simpleMessage("اپنے ہاتھ بار بار دھوئے"),
+    "whoMythBusters" : MessageLookupByLibrary.simpleMessage("ڈبلیو ایچ او میتھ بسٹرز"),
+    "worldHealthOrganization" : MessageLookupByLibrary.simpleMessage("عالمی ادارہ صحت")
+  };
+}
