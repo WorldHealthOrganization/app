5b06ebd949202be2cccc214d4740cf71c904eeed
mirankm <miran9191@gmail.com>
2020-03-27 04:56:27 +0300

    adds few kurdish translation

--- client/flutter/lib/generated/intl/messages_ku.dart
+++ client/flutter/lib/generated/intl/messages_ku.dart
@@ -21,9 +21,9 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static _notInlinedMessages(_) => <String, Function> {
-    "aboutTheApp" : MessageLookupByLibrary.simpleMessage("عن التطبيق"),
-    "avoidTouching" : MessageLookupByLibrary.simpleMessage("تجنب لمس عينيك وفمك وأنفك"),
-    "checkYourHealth" : MessageLookupByLibrary.simpleMessage("افحص صحتك"),
+    "aboutTheApp" : MessageLookupByLibrary.simpleMessage("دەربارەی نەرماڵە"),
+    "avoidTouching" : MessageLookupByLibrary.simpleMessage("دوربە لە دەستدان لە چاوت و لووتت و دەمت"),
+    "checkYourHealth" : MessageLookupByLibrary.simpleMessage("تەندروستی خۆت بپشكنە"),
     "childDistance" : MessageLookupByLibrary.simpleMessage("حاول إبقاء الأطفال على مقربة من والديهم وعائلتهم وتجنب فصل الأطفال ومقدمي الرعاية لهم قدر الإمكان. في حالة حدوث الانفصال (مثل الاستشفاء) ، تأكد من الاتصال المنتظم (على سبيل المثال عبر الهاتف) وإعادة التأكيد."),
     "childFacts" : MessageLookupByLibrary.simpleMessage("قدم حقائق حول ما حدث ، واشرح ما يجري الآن وأعطهم معلومات واضحة حول كيفية تقليل خطر الإصابة بالمرض في الكلمات التي يمكنهم فهمها اعتمادًا على أعمارهم.\nيشمل ذلك أيضًا تقديم معلومات حول ما يمكن أن يحدث بطريقة إعادة التأكيد (على سبيل المثال ، قد لا يشعر أحد أفراد العائلة و / أو الطفل بحالة جيدة وقد يضطر إلى الذهاب إلى المستشفى لبعض الوقت حتى يتمكن الأطباء من مساعدتهم على الشعور بتحسن)."),
     "childLove" : MessageLookupByLibrary.simpleMessage("يحتاج الأطفال إلى حب واهتمام البالغين في الأوقات الصعبة. امنحهم المزيد من الوقت والاهتمام.\nتذكر الاستماع إلى أطفالك ، والتحدث بلطف وطمأنينة.\nإذا كان ذلك ممكنًا ، اجعل الفرص للطفل للعب والاسترخاء."),
@@ -31,21 +31,21 @@ class MessageLookup extends MessageLookupByLibrary {
     "childStress" : MessageLookupByLibrary.simpleMessage("قد يستجيب الأطفال للتوتر بطرق مختلفة مثل أن يكونوا أكثر عنادا أو قلقًا أو انسحابًا أو غضبًا أو التبول اللاإرادي إلخ\nاستجب لردود أفعال طفلك بطريقة داعمة واستمع إلى مخاوفه وأعطه المزيد من الحب والاهتمام."),
     "cougningAndSneezing" : MessageLookupByLibrary.simpleMessage("عند السعال والعطس غطي الفم والأنف بكوع أو بمنديل"),
     "coverUrMouth" : MessageLookupByLibrary.simpleMessage("قم بتغطية فمك وأنفك بمرفقك أو منديلك عند السعال أو العطس"),
-    "feelingDistressed" : MessageLookupByLibrary.simpleMessage("تشعر بذعر؟"),
-    "healthCheck" : MessageLookupByLibrary.simpleMessage("فحص الصحة"),
-    "infoForEveryone" : MessageLookupByLibrary.simpleMessage("معلومات للجميع"),
-    "infoForParents" : MessageLookupByLibrary.simpleMessage("معلومات للوالدين"),
-    "learnMore" : MessageLookupByLibrary.simpleMessage("تعلم أكثر"),
-    "localMaps" : MessageLookupByLibrary.simpleMessage("خرائط محلية"),
-    "protectYourself" : MessageLookupByLibrary.simpleMessage("احمي نفسك"),
+    "feelingDistressed" : MessageLookupByLibrary.simpleMessage("هەست بە پەشۆكان ئەكەی؟"),
+    "healthCheck" : MessageLookupByLibrary.simpleMessage("پشكنینی تەندروستی"),
+    "infoForEveryone" : MessageLookupByLibrary.simpleMessage("زانیاری بۆ هەمووان"),
+    "infoForParents" : MessageLookupByLibrary.simpleMessage("زانیاری بۆ دایك و باوك"),
+    "learnMore" : MessageLookupByLibrary.simpleMessage("زیاتر فێرببە"),
+    "localMaps" : MessageLookupByLibrary.simpleMessage("نەخشەى ناوچە"),
+    "protectYourself" : MessageLookupByLibrary.simpleMessage("خۆت بپارێزە"),
     "seekMedicalCare" : MessageLookupByLibrary.simpleMessage("اطلب الرعاية الطبية بسرعة إذا كنت تعاني من الحمى والسعال وصعوبة التنفس"),
-    "shareTheApp" : MessageLookupByLibrary.simpleMessage("شارك التطبيق"),
+    "shareTheApp" : MessageLookupByLibrary.simpleMessage("نەرماڵەكە بڵاو بكەوە"),
     "socialDistancing" : MessageLookupByLibrary.simpleMessage("تجنب الاتصال قريب وحافظ على مسافة من افراد"),
     "throwAwayTissue" : MessageLookupByLibrary.simpleMessage("ارمي المنديل في حاوية مغلقة مباشرة بعد الاستخدام"),
-    "travelAdvice" : MessageLookupByLibrary.simpleMessage("نصائح السفر"),
+    "travelAdvice" : MessageLookupByLibrary.simpleMessage("ئامۆژگارى سەفەر"),
     "washHands" : MessageLookupByLibrary.simpleMessage("اغسل ايديك كثيرًا بالصابون والماء الجاري بشكل متكرر"),
-    "washHandsFrequently" : MessageLookupByLibrary.simpleMessage("اغسل يديك باستمرار"),
-    "whoMythBusters" : MessageLookupByLibrary.simpleMessage("منظمة الصحة العالمية منتهكي الأسطورة"),
-    "worldHealthOrganization" : MessageLookupByLibrary.simpleMessage("منظمة الصحة العالمية")
+    "washHandsFrequently" : MessageLookupByLibrary.simpleMessage("دەسەكانت بە بەردەوامی بشۆ"),
+    "whoMythBusters" : MessageLookupByLibrary.simpleMessage("ڕیخراوی تەندروستی جیهانی ئەفسانە شكێن"),
+    "worldHealthOrganization" : MessageLookupByLibrary.simpleMessage("ڕێكخراو تەندروستی جیعانی")
   };
 }
