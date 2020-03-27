12b02420754e1f16c0f9c104d61544a55e8f2fa0
KoheiKanagu <kanagu@kingu.dev>
2020-03-27 15:26:23 +0900

    add ja translation

---
+++ client/flutter/lib/generated/intl/messages_ja.dart
@@ -0,0 +1,49 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a zh locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ja';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "aboutTheApp" : MessageLookupByLibrary.simpleMessage("アプリについて"),
+    "checkYourHealth" : MessageLookupByLibrary.simpleMessage("あなたの健康をチェックする"),
+    "childDistance" : MessageLookupByLibrary.simpleMessage("親や家族との距離を縮め、子どもと保育者が離れないようにしましょう。もし(入院などで)離れ離れになった場合は、(電話などで)定期的に連絡を取り、安心させましょう。"),
+    "childFacts" : MessageLookupByLibrary.simpleMessage("何が起こったのか、今何が起こっているのかを説明し、病気に感染するリスクをどのように減らすかについて年齢に応じて理解できる言葉で正確な情報を与えましょう。これには、安心させるために起こりうることについての情報を提供することも含まれています。（例えば、家族や子供が具合が悪くなり始め、しばらくの間病院に行かなければならない時に医師に助けてもらうことができます）"),
+    "childLove" : MessageLookupByLibrary.simpleMessage("辛い時、子供たちには大人の愛情が必要です。十分な時間、気にかけてあげてください。子どもの話に耳を傾け、優しく話し、大切にすることを忘れないようにしましょう。できれば子どもが遊んだりリラックスしたりする機会を作ってあげましょう。"),
+    "childRoutines" : MessageLookupByLibrary.simpleMessage("できるだけ規則正しい生活習慣やスケジュールを守ったり、学校や自習だけでなく、安全に遊んだりくつろいだりする時間も含めて新しい環境の中で新しいものを生み出すお手伝いをしましょう。"),
+    "childStress" : MessageLookupByLibrary.simpleMessage("子供たちはストレスに対していつもより甘えてきたり、不安になったり、引きこもったり、怒ったり興奮したり、おねしょをしたりなど、さまざまな反応を示すことがあります。"),
+    "cougningAndSneezing" : MessageLookupByLibrary.simpleMessage("咳やくしゃみをするときは、曲げた肘やティッシュで口と鼻を覆う"),
+    "feelingDistressed" : MessageLookupByLibrary.simpleMessage("悩んでいますか？"),
+    "healthCheck" : MessageLookupByLibrary.simpleMessage("ヘルスチェック"),
+    "infoForEveryone" : MessageLookupByLibrary.simpleMessage("皆さんへの情報"),
+    "infoForParents" : MessageLookupByLibrary.simpleMessage("保護者への情報"),
+    "learnMore" : MessageLookupByLibrary.simpleMessage("もっと詳しく"),
+    "localMaps" : MessageLookupByLibrary.simpleMessage("ローカルマップ"),
+    "protectYourself" : MessageLookupByLibrary.simpleMessage("自分を守る"),
+    "seekMedicalCare" : MessageLookupByLibrary.simpleMessage("発熱、咳、呼吸困難の場合は、早めに医療機関を受診してください"),
+    "shareTheApp" : MessageLookupByLibrary.simpleMessage("アプリを共有する"),
+    "socialDistancing" : MessageLookupByLibrary.simpleMessage("密接な接触を避け、物理的な距離を保つ"),
+    "throwAwayTissue" : MessageLookupByLibrary.simpleMessage("使用後すぐに蓋を閉じたゴミ箱にティッシュを捨てる"),
+    "travelAdvice" : MessageLookupByLibrary.simpleMessage("旅行のアドバイス"),
+    "washHands" : MessageLookupByLibrary.simpleMessage("石鹸と流水でこまめに手を洗う"),
+    "washHandsFrequently" : MessageLookupByLibrary.simpleMessage("手を頻繁に洗う"),
+    "whoMythBusters" : MessageLookupByLibrary.simpleMessage("WHO 怪しい伝説"),
+    "worldHealthOrganization" : MessageLookupByLibrary.simpleMessage("世界保健機関")
+  };
+}
