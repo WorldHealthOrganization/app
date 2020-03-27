e17f290647b6e921aade453ab4ee369506ea7e19
gcarmix <carmixdev@gmail.com>
2020-03-27 17:54:08 +0100

    Added italian content to app, resolves issue #401

---
+++ client/flutter/lib/generated/intl/messages_it.dart
@@ -0,0 +1,49 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a it locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'it';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "aboutTheApp" : MessageLookupByLibrary.simpleMessage("Informazioni sull\' App"),
+    "checkYourHealth" : MessageLookupByLibrary.simpleMessage("Controlla la tua Salute"),
+    "childDistance" : MessageLookupByLibrary.simpleMessage("Cercare di tenere i bambini vicino ai loro genitori e alla famiglia e evitare il più possibile di separare i bambini da chi si prende cura di loro. Se la separazione è necessaria (es. ospedalizzazione) assicurarsi di un contatto regolare (es. telefono) e rassicurazioni."),
+    "childFacts" : MessageLookupByLibrary.simpleMessage("Dai informazioni su quello che è successo, spiega cosa si sta facendo ora e da loro informazioni chiare su come ridurre il rischio di essere contagiati dalla malattia con parole che possono capire in base alla loro età.\nQuesto include fornire in modo rassicurante informazioni su cosa potrebbe succedere (es. un membro della famiglia e/o il bambino potrebbero iniziare a non sentirsi bene e potrebbero dover andare all\'ospedale per un po\' di tempo così i dottori possono aiutarli a stare meglio)."),
+    "childLove" : MessageLookupByLibrary.simpleMessage("I bambini hanno bisogno dell\' amore degli adulti e di attenzione nei momenti difficili. Concedi loro più tempo e attenzione.\nRicordati di ascoltare il tuo bambino, parla gentilmente e rassicuralo.\nSe possibile crea per il bambino momenti di gioco e di relax."),
+    "childRoutines" : MessageLookupByLibrary.simpleMessage("Mantenere le abitudini e gli impegni il più possibile, o aiuta a crearne di nuove in un nuovo ambiente, inclusi scuola/apprendimento così come il tempo per giocare in sicurezza e rilassarsi."),
+    "childStress" : MessageLookupByLibrary.simpleMessage("I bambini possono reagire allo stress in modi differenti così da essere più bisognosi di attenzioni, ansiosi, timidi, scontrosi o agitati, bagnare il letto ecc...Rispondi alle reazioni del tuo bambino in maniera comprensiva, ascolta le sue preoccupazioni e dagli più amore e attenzioni."),
+    "cougningAndSneezing" : MessageLookupByLibrary.simpleMessage("Quando tossisci o starnutisci copriti la bocca con il gomito o con un fazzoletto"),
+    "feelingDistressed" : MessageLookupByLibrary.simpleMessage("Ti senti angosciato?"),
+    "healthCheck" : MessageLookupByLibrary.simpleMessage("Controllo della Salute"),
+    "infoForEveryone" : MessageLookupByLibrary.simpleMessage("Informazioni per tutti"),
+    "infoForParents" : MessageLookupByLibrary.simpleMessage("Informazioni per i genitori"),
+    "learnMore" : MessageLookupByLibrary.simpleMessage("Approfondisci"),
+    "localMaps" : MessageLookupByLibrary.simpleMessage("Mappe locali"),
+    "protectYourself" : MessageLookupByLibrary.simpleMessage("Proteggiti"),
+    "seekMedicalCare" : MessageLookupByLibrary.simpleMessage("Contatta subito il supporto medico se hai febbre, tosse e difficoltà respiratorie"),
+    "shareTheApp" : MessageLookupByLibrary.simpleMessage("Condividi l\'App"),
+    "socialDistancing" : MessageLookupByLibrary.simpleMessage("Evita il contatto ravvicinato e mantieni le distanze"),
+    "throwAwayTissue" : MessageLookupByLibrary.simpleMessage("Getta il fazzoletto in un secchio chiuso immediatamente dopo l\'uso"),
+    "travelAdvice" : MessageLookupByLibrary.simpleMessage("Avvisi per i viaggi"),
+    "washHands" : MessageLookupByLibrary.simpleMessage("Lavati le mani spesso con sapone e acqua corrente"),
+    "washHandsFrequently" : MessageLookupByLibrary.simpleMessage("Lavati le mani frequentemente"),
+    "whoMythBusters" : MessageLookupByLibrary.simpleMessage("WHO - Miti da sfatare"),
+    "worldHealthOrganization" : MessageLookupByLibrary.simpleMessage("Organizzazione Mondiale della Sanità")
+  };
+}
