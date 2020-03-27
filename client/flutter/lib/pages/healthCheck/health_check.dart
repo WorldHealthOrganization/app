ffc2a5e4064c4b53031273e152e31e27eeed51d9
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-03-27 14:35:12 -0700

    Updated home page

--- client/flutter/lib/pages/healthCheck/health_check.dart
+++ client/flutter/lib/pages/healthCheck/health_check.dart
@@ -1,11 +1,10 @@
 import 'package:WHOFlutter/generated/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:WHOFlutter/page_scaffold.dart';
 
 class HealthCheckPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageScaffold(
-        body: Text(S.of(context).healthCheck));
+    return Material(
+        child: Text(S.of(context).healthCheck));
   }
 }
