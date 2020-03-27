0d9d66f3fbd5cb47fbc1c8ca48a78960b5b191c6
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-27 13:01:06 -0700

    Changed Home Page

--- client/flutter/lib/home_page.dart
+++ client/flutter/lib/home_page.dart
@@ -7,43 +7,89 @@ import 'package:WHOFlutter/pages/who_myth_busters.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
+  const StaggeredTile.count(1, 2),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(1, 1),
+  const StaggeredTile.count(2, 1),
+];
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageScaffold(
-        body: Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Spacer(flex: 1),
+        body: new StaggeredGridView.count(
+      crossAxisCount: 2,
+      staggeredTiles: _staggeredTiles,
+      children: [
         PageButton(
-            title: S.of(context).protectYourself,
-            onPressed: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (c) => ProtectYourself()))),
+          Colors.green,
+          S.of(context).protectYourself,
+          () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (c) => ProtectYourself())),
+        ),
         PageButton(
-            title: S.of(context).whoMythBusters,
-            onPressed: () => Navigator.of(context)
+          Colors.lightBlue,
+          "Latest Numbers",
+          () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (c) => ProtectYourself())),
+        ),
+        PageButton(
+          Colors.amber,
+          "Your Questions Answered",
+          () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (c) => ProtectYourself())),
+        ),
+        PageButton(
+            Colors.brown,
+            S.of(context).whoMythBusters,
+            () => Navigator.of(context)
                 .push(MaterialPageRoute(builder: (c) => WhoMythBusters()))),
-        PageButton(
-            title: S.of(context).travelAdvice,
-            onPressed: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (c) => TravelAdvice()))),
-        PageButton(
-            title: S.of(context).shareTheApp,
-            lightColor: true,
-            onPressed: () => Share.share(
-                'Check out the official COVID-19 Guide App https://www.who.int/covid-19-app')),
-        PageButton(
-            title: S.of(context).aboutTheApp,
-            lightColor: true,
-            onPressed: () => showAboutDialog(
-                context: context,
-                applicationLegalese:
-                    "The official World Health Organization COVID-19 App.")),
-        Spacer(flex: 3)
       ],
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+      padding: const EdgeInsets.all(4.0),
     ));
+
+    // PageButton(
+    //     title: S.of(context).travelAdvice,
+    //     onPressed: () => Navigator.of(context)
+    //         .push(MaterialPageRoute(builder: (c) => TravelAdvice()))),
+    // PageButton(
+    //     title: S.of(context).shareTheApp,
+    //     lightColor: true,
+    //     onPressed: () => Share.share(
+    //         'Check out the official COVID-19 Guide App https://www.who.int/covid-19-app')),
+    // PageButton(
+    //     title: S.of(context).aboutTheApp,
+    //     lightColor: true,
+    //     onPressed: () => showAboutDialog(
+    //         context: context,
+    //         applicationLegalese:
+    //             "The official World Health Organization COVID-19 App.")),
+  }
+}
+
+class PageButton extends StatelessWidget {
+  const PageButton(this.backgroundColor, this.title, this.onPressed);
+
+  final Color backgroundColor;
+  final String title;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      color: backgroundColor,
+      child: new InkWell(
+        onTap: () {},
+        child: new Center(
+          child: new Padding(
+              padding: const EdgeInsets.all(4.0), child: new Text(title)),
+        ),
+      ),
+    );
   }
 }
