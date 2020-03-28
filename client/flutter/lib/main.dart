e917c511369da08a97c1dbcb8c84d555428e22ec
HirushaR <hirusharandunu11@gmail.com>
2020-03-29 01:32:18 +0530

    remove the debuger banner

--- client/flutter/lib/main.dart
+++ client/flutter/lib/main.dart
@@ -23,6 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "WHO",
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
