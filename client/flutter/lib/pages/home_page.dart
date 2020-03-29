ceb32e23800bcf8f4e175a05cf5513759931dcf3
emilsharier <emilsharier@gmail.com>
2020-03-29 09:59:44 +0530

    Adding permission and bottom sheet to prompt location access from user

--- client/flutter/lib/pages/home_page.dart
+++ client/flutter/lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:WHOFlutter/api/user_preferences.dart';
 import 'package:WHOFlutter/components/page_button.dart';
 import 'package:WHOFlutter/pages/question_index.dart';
@@ -9,6 +11,7 @@ import 'package:WHOFlutter/pages/who_myth_busters.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
@@ -24,12 +27,29 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _initStateAsync() async {
-    var onboardingComplete = await UserPreferences().getOnboardingCompleted();
-    if (!onboardingComplete) {
+    //var onboardingComplete = await UserPreferences().//getOnboardingCompleted();
+    //print('Onbardingcomplete : ' + onboardingComplete.toString());
+    
+    //Checking permission directly in the _initStateAsync() function
+    PermissionStatus _status = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.location);
+    print(_status.value.toString());
+    if (_status.value == 0) {
+      
       // TODO: This should be a bottom-up slide.
-      await Navigator.of(context)
-          .push(MaterialPageRoute(builder: (c) => LocationSharing()));
-      await UserPreferences().setOnboardingCompleted(true);
+      
+      //created a Bottom Up slide that will pop up everytime home page is triggered if the user hasn't given location access
+      await showModalBottomSheet(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          )),
+          context: context,
+          builder: (context) {
+            return LocationSharing();
+          });
+      // await UserPreferences().setOnboardingCompleted(true);
     }
   }
 
@@ -39,81 +59,86 @@ class _HomePageState extends State<HomePage> {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0),
         child: SafeArea(
-          child: CustomScrollView(slivers: [
-            SliverAppBar(
-                expandedHeight: 110,
-                backgroundColor: Colors.white,
-                flexibleSpace: Image.asset("assets/WHO.jpg")),
-            SliverStaggeredGrid.count(
-              crossAxisCount: 2,
-              staggeredTiles: [
-                StaggeredTile.count(1, 2),
-                StaggeredTile.count(1, 1),
-                StaggeredTile.count(1, 1),
-                StaggeredTile.count(2, 1),
-                StaggeredTile.count(2, .5),
-              ],
-              children: [
-                PageButton(
-                  Color(0xff3b8bc4),
-                  S.of(context).homePagePageButtonProtectYourself,
-                  () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (c) => ProtectYourself())),
+          child: CustomScrollView(
+              physics:
+                  (Platform.isIOS) ? BouncingScrollPhysics() : ScrollPhysics(),
+              slivers: [
+                SliverAppBar(
+                    expandedHeight: 110,
+                    backgroundColor: Colors.white,
+                    flexibleSpace: Image.asset("assets/WHO.jpg")),
+                SliverStaggeredGrid.count(
+                  crossAxisCount: 2,
+                  staggeredTiles: [
+                    StaggeredTile.count(1, 2),
+                    StaggeredTile.count(1, 1),
+                    StaggeredTile.count(1, 1),
+                    StaggeredTile.count(2, 1),
+                    StaggeredTile.count(2, .5),
+                  ],
+                  children: [
+                    PageButton(
+                      Color(0xff3b8bc4),
+                      S.of(context).homePagePageButtonProtectYourself,
+                      () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (c) => ProtectYourself())),
+                    ),
+                    PageButton(
+                      Color(0xfff6c35c),
+                      S.of(context).homePagePageButtonLatestNumbers,
+                      () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (c) => ProtectYourself())),
+                    ),
+                    PageButton(
+                      Color(0xffbe7141),
+                      S.of(context).homePagePageButtonYourQuestionsAnswered,
+                      () => Navigator.of(context).push(MaterialPageRoute(
+                          builder: (c) => QuestionIndexPage())),
+                    ),
+                    PageButton(
+                      Color(0xff234689),
+                      S.of(context).homePagePageButtonWHOMythBusters,
+                      () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (c) => WhoMythBusters()),
+                      ),
+                      description:
+                          "Learn the facts about Coronavirus and how to prevent the spread",
+                      centerVertical: true,
+                    ),
+                    PageButton(
+                      Color(0xffba4344),
+                      S.of(context).homePagePageButtonTravelAdvice,
+                      () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (c) => TravelAdvice())),
+                      borderRadius: 50,
+                      centerVertical: true,
+                    ),
+                  ],
+                  mainAxisSpacing: 15.0,
+                  crossAxisSpacing: 15.0,
                 ),
-                PageButton(
-                  Color(0xfff6c35c),
-                  S.of(context).homePagePageButtonLatestNumbers,
-                  () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (c) => ProtectYourself())),
+                SliverList(
+                  delegate: SliverChildListDelegate.fixed([
+                    ListTile(
+                      title:
+                          Text(S.of(context).homePagePageSliverListShareTheApp),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () => Share.share(
+                          'Check out the official COVID-19 Guide App https://www.who.int/covid-19-app'),
+                    ),
+                    ListTile(
+                      title:
+                          Text(S.of(context).homePagePageSliverListAboutTheApp),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () => showAboutDialog(
+                          context: context,
+                          applicationLegalese: S
+                              .of(context)
+                              .homePagePageSliverListAboutTheAppDialog),
+                    )
+                  ]),
                 ),
-                PageButton(
-                  Color(0xffbe7141),
-                  S.of(context).homePagePageButtonYourQuestionsAnswered,
-                  () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (c) => QuestionIndexPage())),
-                ),
-                PageButton(
-                  Color(0xff234689),
-                  S.of(context).homePagePageButtonWHOMythBusters,
-                  () => Navigator.of(context).push(
-                    MaterialPageRoute(builder: (c) => WhoMythBusters()),
-                  ),
-                  description:
-                      "Learn the facts about Coronavirus and how to prevent the spread",
-                  centerVertical: true,
-                ),
-                PageButton(
-                  Color(0xffba4344),
-                  S.of(context).homePagePageButtonTravelAdvice,
-                  () => Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (c) => TravelAdvice())),
-                  borderRadius: 50,
-                  centerVertical: true,
-                ),
-              ],
-              mainAxisSpacing: 15.0,
-              crossAxisSpacing: 15.0,
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate.fixed([
-                ListTile(
-                  title: Text(S.of(context).homePagePageSliverListShareTheApp),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => Share.share(
-                      'Check out the official COVID-19 Guide App https://www.who.int/covid-19-app'),
-                ),
-                ListTile(
-                  title: Text(S.of(context).homePagePageSliverListAboutTheApp),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () => showAboutDialog(
-                      context: context,
-                      applicationLegalese: S
-                          .of(context)
-                          .homePagePageSliverListAboutTheAppDialog),
-                )
               ]),
-            ),
-          ]),
         ),
       ),
     );
--- client/flutter/lib/pages/onboarding/location_sharing.dart
+++ client/flutter/lib/pages/onboarding/location_sharing.dart
@@ -1,5 +1,8 @@
+import 'package:WHOFlutter/api/user_preferences.dart';
 import 'package:WHOFlutter/components/page_button.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocationSharing extends StatefulWidget {
   @override
@@ -7,52 +10,69 @@ class LocationSharing extends StatefulWidget {
 }
 
 class _LocationSharingState extends State<LocationSharing> {
+  Map<PermissionGroup, PermissionStatus> permissions;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(48.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Spacer(),
-              Image.asset("assets/WHO.jpg"),
-              Spacer(),
-              PageButton(
-                Color(0xff3b8bc4),
-                "Allow Location Sharing",
-                _allowLocationSharing,
-                titleStyle: TextStyle(),
-                centerVertical: true,
-                centerHorizontal: true,
+      body: Container(
+        // height: 250.0,
+        color: Colors.transparent,
+        padding: EdgeInsets.symmetric(
+          horizontal: 50.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Spacer(
+              flex: 1,
+            ),
+            Image.asset("assets/WHO.jpg"),
+            Spacer(
+              flex: 1,
+            ),
+            PageButton(
+              Color(0xff3b8bc4),
+              "Allow Location Sharing",
+              _allowLocationSharing,
+              titleStyle: TextStyle(),
+              centerVertical: true,
+              centerHorizontal: true,
+            ),
+            FlatButton(
+              child: Text(
+                "Skip",
+                style: TextStyle(color: Colors.grey),
               ),
-              SizedBox(height: 16),
-              FlatButton(
-                child: Text(
-                  "Skip",
-                  style: TextStyle(color: Colors.grey),
-                ),
-                onPressed: _skipLocationSharing,
-              ),
-              Spacer(),
-            ],
-          ),
+              onPressed: _skipLocationSharing,
+            ),
+          ],
         ),
       ),
     );
   }
 
   void _allowLocationSharing() async {
+    permissions = await PermissionHandler()
+        .requestPermissions([PermissionGroup.location]);
+    ServiceStatus serviceStatus =
+        await PermissionHandler().checkServiceStatus(PermissionGroup.location);
+    if (serviceStatus.value == 1) {
+      await UserPreferences().setOnboardingCompleted(true);
+    } else {
+      await UserPreferences().setOnboardingCompleted(false);
+    }
     _complete();
   }
 
   void _skipLocationSharing() async {
+    await UserPreferences().setOnboardingCompleted(false);
     _complete();
   }
 
   void _complete() async {
+    var value = await UserPreferences().getOnboardingCompleted();
+    print(value.toString());
     Navigator.of(context).pop();
   }
 }
