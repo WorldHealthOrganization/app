60c4504695dc378a9c0a31fb2947e17f624769a7
Pat Niemeyer <pat@pat.net>
2020-03-30 17:15:24 -0500

    Remove stale comments and format. (#663)

--- client/flutter/lib/pages/question_index.dart
+++ client/flutter/lib/pages/question_index.dart
@@ -35,15 +35,14 @@ class _QuestionIndexPageState extends State<QuestionIndexPage> {
   @override
   void didChangeDependencies() async {
     super.didChangeDependencies();
+
+    // Note: this depends on the build context for the locale and hence is not
+    // Note: available at the usual initState() time.
     await _loadQuestionData();
   }
 
   Future _loadQuestionData() async {
     // Fetch the question data.
-    // Note: this depends on the build context for the locale and hence is not
-    // Note: available at the usual initState() time.
-    // TODO: We should detect a schema version problem here and display a dialog
-    // TODO: prompting the user to upgrade.
     if (_questions != null) {
       return;
     }
@@ -56,7 +55,6 @@ class _QuestionIndexPageState extends State<QuestionIndexPage> {
     return Scaffold(body: _buildPage());
   }
 
-  // TODO: Should show a spinner while loading.
   Widget _buildPage() {
     List items = (_questions ?? [])
         .map((questionData) => QuestionTile(
@@ -101,9 +99,10 @@ class _QuestionTileState extends State<QuestionTile>
   void initState() {
     super.initState();
     rotationController = AnimationController(
-        duration: const Duration(milliseconds: 200), vsync: this, lowerBound: 0, upperBound: pi/4);
-
-    
+        duration: const Duration(milliseconds: 200),
+        vsync: this,
+        lowerBound: 0,
+        upperBound: pi / 4);
   }
 
   @override
@@ -116,9 +115,9 @@ class _QuestionTileState extends State<QuestionTile>
         ),
         ExpansionTile(
           onExpansionChanged: (expanded) {
-            if(expanded){
+            if (expanded) {
               rotationController.forward();
-            }else{
+            } else {
               rotationController.reverse();
             }
           },
@@ -126,8 +125,11 @@ class _QuestionTileState extends State<QuestionTile>
           trailing: AnimatedBuilder(
             animation: rotationController,
             child: Icon(Icons.add_circle_outline, color: Colors.grey),
-            builder: (context, child){
-              return Transform.rotate(angle: rotationController.value, child: child,);
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: rotationController.value,
+                child: child,
+              );
             },
           ),
           title: html(widget.questionItem.title),
