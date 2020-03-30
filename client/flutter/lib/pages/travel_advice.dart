058694098a813b302f16284b86b8fe66eec36b20
jasonTelanoff <jason.telanoff@gmail.com>
2020-03-29 21:38:08 -0700

    Removed unnecessary imports

--- client/flutter/lib/pages/travel_advice.dart
+++ client/flutter/lib/pages/travel_advice.dart
@@ -1,5 +1,3 @@
-import 'package:WHOFlutter/generated/l10n.dart';
-import 'package:WHOFlutter/components/list_of_items.dart';
 import 'package:flutter/material.dart';
 import 'package:WHOFlutter/constants.dart';
 
