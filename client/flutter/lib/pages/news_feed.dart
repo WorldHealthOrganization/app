7d214559619541b59050658de03cef61c5d010fa
Britannio <britanniojarrett@gmail.com>
2020-03-30 16:21:34 +0100

    refactor: add correct urls to news feed

--- client/flutter/lib/pages/news_feed.dart
+++ client/flutter/lib/pages/news_feed.dart
@@ -1,5 +1,3 @@
-//TODO: ENTER CORRECT URL FOR EACH ITEM
-
 import 'package:WHOFlutter/components/news_feed_item.dart';
 import 'package:WHOFlutter/components/page_scaffold.dart';
 import 'package:flutter/material.dart';
@@ -18,28 +16,32 @@ class NewsFeed extends StatelessWidget {
                 "Situation reports provide the latest updates on the novel coronavirus outbreak.",
             imageProvider:
                 AssetImage("assets/news_press/news_situation_reports.png"),
-            url: "https://who.int",
+            url:
+                "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports/",
           ),
           NewsFeedItem(
             title: "Rolling Updates",
             description:
                 "Rolling updates on coronavirus disease (COVID-19) sourced from across WHO media.",
             imageProvider: AssetImage("assets/news_press/rolling_updates.png"),
-            url: "https://who.int",
+            url:
+                "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/events-as-they-happen",
           ),
           NewsFeedItem(
             title: "News Articles",
             description:
                 "All news releases, statements, and notes for the media.",
             imageProvider: AssetImage("assets/news_press/news_articles.png"),
-            url: "https://who.int",
+            url:
+                "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/news",
           ),
           NewsFeedItem(
             title: "Press Briefings",
             description:
                 "Coronavirus disease (COVID019) press briefings including videos, audio and transcripts.",
             imageProvider: AssetImage("assets/news_press/press_briefings.png"),
-            url: "https://who.int",
+            url:
+                "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/press-briefings",
           )
         ]))
       ],
