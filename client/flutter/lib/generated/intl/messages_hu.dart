b648bb0b1c8917d065b35378f2e61e2b9944fe57
Kiripolszky Károly <karcsi@ekezet.com>
2020-03-31 16:33:45 +0200

    Hungarian translation

---
+++ client/flutter/lib/generated/intl/messages_hu.dart
@@ -0,0 +1,93 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a hu locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'hu';
+
+  static m2(version, buildNumber) => "Verzió 0.1 (12412)\n© 2020 WHO";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "commonPermissionRequestPageButtonSkip" : MessageLookupByLibrary.simpleMessage("Kihagyás"),
+    "commonWhoAppShareIconButtonDescription" : MessageLookupByLibrary.simpleMessage("Próbáld ki a hivatalos COVID-19 Kalauz applikációt https://whocoronavirus.org/app"),
+    "commonWorldHealthOrganization" : MessageLookupByLibrary.simpleMessage("Egészségügyi Világszervezet"),
+    "commonWorldHealthOrganizationCoronavirusApp" : MessageLookupByLibrary.simpleMessage("COVID-19 App"),
+    "commonWorldHealthOrganizationCoronavirusAppVersion" : m2,
+    "healthCheckTitle" : MessageLookupByLibrary.simpleMessage("Health Check"),
+    "homePagePageButtonLatestNumbers" : MessageLookupByLibrary.simpleMessage("Friss \nszámok"),
+    "homePagePageButtonLatestNumbersUrl" : MessageLookupByLibrary.simpleMessage("https://experience.arcgis.com/experience/685d0ace521648f8a5beeeee1b9125cd"),
+    "homePagePageButtonProtectYourself" : MessageLookupByLibrary.simpleMessage("Védekezz!"),
+    "homePagePageButtonQuestions" : MessageLookupByLibrary.simpleMessage("Kérdések"),
+    "homePagePageButtonTravelAdvice" : MessageLookupByLibrary.simpleMessage("Utazási tippek"),
+    "homePagePageButtonWHOMythBusters" : MessageLookupByLibrary.simpleMessage("WHO Igaz vagy hamis?"),
+    "homePagePageButtonWHOMythBustersDescription" : MessageLookupByLibrary.simpleMessage("Ismerd meg a koronavírussal kapcsolatos tényeket és hogy miként lassítható a terjedése"),
+    "homePagePageButtonYourQuestionsAnswered" : MessageLookupByLibrary.simpleMessage("Kérdések \nés válaszok"),
+    "homePagePageSliverListAboutTheApp" : MessageLookupByLibrary.simpleMessage("Az appról"),
+    "homePagePageSliverListAboutTheAppDialog" : MessageLookupByLibrary.simpleMessage("A hivatalos WHO COVID-19 Kalauz applikáció."),
+    "homePagePageSliverListDonate" : MessageLookupByLibrary.simpleMessage("Adományozás"),
+    "homePagePageSliverListDonateUrl" : MessageLookupByLibrary.simpleMessage("https://www.who.int/Covid19ResponseFund"),
+    "homePagePageSliverListProvideFeedback" : MessageLookupByLibrary.simpleMessage("Értékeld az appot!"),
+    "homePagePageSliverListSettings" : MessageLookupByLibrary.simpleMessage("Beállítások"),
+    "homePagePageSliverListSettingsDataCollection" : MessageLookupByLibrary.simpleMessage("A hatékonyabb termékfejlesztés érdekében engedélyezem a WHO számára, hogy adatokat gyűjtsön a Google Analytics segítségével"),
+    "homePagePageSliverListSettingsHeader1" : MessageLookupByLibrary.simpleMessage("Adatgyűjtés"),
+    "homePagePageSliverListSettingsHeader2" : MessageLookupByLibrary.simpleMessage("Nyelv"),
+    "homePagePageSliverListShareTheApp" : MessageLookupByLibrary.simpleMessage("Oszd meg!"),
+    "homePagePageSliverListSupport" : MessageLookupByLibrary.simpleMessage("Segítsd munkánkat"),
+    "homePagePageSubTitle" : MessageLookupByLibrary.simpleMessage("Hasznos információk"),
+    "homePagePageTitle" : MessageLookupByLibrary.simpleMessage("Koronavírus"),
+    "newsFeedSliverListNewsFeedItemDescription1" : MessageLookupByLibrary.simpleMessage("A legfrissebb helyzetjelentések az új koronavírus-járvánnyal kapcsolatban"),
+    "newsFeedSliverListNewsFeedItemDescription2" : MessageLookupByLibrary.simpleMessage("A WHO naprakész információi az új koronavírus-fertőzéssel (COVID-19) kapcsolatban."),
+    "newsFeedSliverListNewsFeedItemDescription3" : MessageLookupByLibrary.simpleMessage("Új hírek, sajtóközlemények és egyéb anyagok."),
+    "newsFeedSliverListNewsFeedItemDescription4" : MessageLookupByLibrary.simpleMessage("A koronavírusos megbetegedéssel (COVID-2019) kapcsolatos sajtótájékoztatók, videók, hanganyagok és leiratok."),
+    "newsFeedSliverListNewsFeedItemImagePath1" : MessageLookupByLibrary.simpleMessage("assets/news_press/news_situation_reports.png"),
+    "newsFeedSliverListNewsFeedItemImagePath2" : MessageLookupByLibrary.simpleMessage("assets/news_press/rolling_updates.png"),
+    "newsFeedSliverListNewsFeedItemImagePath3" : MessageLookupByLibrary.simpleMessage("assets/news_press/news_articles.png"),
+    "newsFeedSliverListNewsFeedItemImagePath4" : MessageLookupByLibrary.simpleMessage("assets/news_press/press_briefings.png"),
+    "newsFeedSliverListNewsFeedItemTitle1" : MessageLookupByLibrary.simpleMessage("Beszámolók"),
+    "newsFeedSliverListNewsFeedItemTitle2" : MessageLookupByLibrary.simpleMessage("Friss információk"),
+    "newsFeedSliverListNewsFeedItemTitle3" : MessageLookupByLibrary.simpleMessage("Hírek"),
+    "newsFeedSliverListNewsFeedItemTitle4" : MessageLookupByLibrary.simpleMessage("Sajtótájékoztatók"),
+    "newsFeedSliverListNewsFeedItemUrl1" : MessageLookupByLibrary.simpleMessage("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports/"),
+    "newsFeedSliverListNewsFeedItemUrl2" : MessageLookupByLibrary.simpleMessage("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/events-as-they-happen"),
+    "newsFeedSliverListNewsFeedItemUrl3" : MessageLookupByLibrary.simpleMessage("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/news"),
+    "newsFeedSliverListNewsFeedItemUrl4" : MessageLookupByLibrary.simpleMessage("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/press-briefings"),
+    "newsFeedTitle" : MessageLookupByLibrary.simpleMessage("Hírek és sajtó"),
+    "protectYourselfListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("*Moss kezet* szappannal és melegvízzel legalább 20 másodpercig"),
+    "protectYourselfListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("*Ne nyúlj* a szemedbe, szádba és az orrodhoz"),
+    "protectYourselfListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("Köhögés vagy tüsszentés esetén *takard el az arcod és az orrod* zsebkendővel vagy a ruhád ujjával"),
+    "protectYourselfListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("*Tarts több, mint 1 méter távolságot* beteg személyektől"),
+    "protectYourselfListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("*Csak akkor* viselj maszkot, ha te vagy egy veled kapcsolatban álló személy megbetegedett (különösen köhögés esetén)"),
+    "protectYourselfTitle" : MessageLookupByLibrary.simpleMessage("Védekezz!"),
+    "whoMythBustersListOfItemsPageListItem1" : MessageLookupByLibrary.simpleMessage("Rengeteg téves információ terjed, ezek a tények"),
+    "whoMythBustersListOfItemsPageListItem10" : MessageLookupByLibrary.simpleMessage("A hőmérő a láz megállapítására alkalmas, de a koronavírus-fertőzést NEM LEHET vele megállapítani"),
+    "whoMythBustersListOfItemsPageListItem11" : MessageLookupByLibrary.simpleMessage("Alkohol vagy klór külsőleges alkalmazása NEM pusztítja el a már a szervezetbe jutott vírust"),
+    "whoMythBustersListOfItemsPageListItem12" : MessageLookupByLibrary.simpleMessage("A tüdőgyulladás elleni vakcinák és készítmények NEM nyújtanak védelmet a koronavírus ellen"),
+    "whoMythBustersListOfItemsPageListItem13" : MessageLookupByLibrary.simpleMessage("Arra NINCS bizonyíték, hogy az orrot rendszeresen sóoldottal tisztítva védekezni lehet a koronavírus ellen"),
+    "whoMythBustersListOfItemsPageListItem14" : MessageLookupByLibrary.simpleMessage("A fokhagyma egészséges, de arra NINCS bizonyíték a jelenlegi járvány alapján, hogy védelmet nyújt a koronavírus ellen"),
+    "whoMythBustersListOfItemsPageListItem15" : MessageLookupByLibrary.simpleMessage("Az antibiotikumok NEM használnak a vírusok ellen, csak a baktériumok ellen"),
+    "whoMythBustersListOfItemsPageListItem16" : MessageLookupByLibrary.simpleMessage("Jelenleg NINCS direkt a koronavírus elleni gyógyszer vagy védőoltás"),
+    "whoMythBustersListOfItemsPageListItem2" : MessageLookupByLibrary.simpleMessage("Koronavírus-fertőzés esélyének MINDEN korosztály ki van téve. Idős emberek és meglévő egészségügyi problémával küzdők (mint asztma, cukor- vagy szívbetegség) nagyobb eséllyel kerülnek súlyos állapotba a koronavírus-fertőzés miatt"),
+    "whoMythBustersListOfItemsPageListItem3" : MessageLookupByLibrary.simpleMessage("Hidegvíz vagy hó NEM öli meg a koronavírust"),
+    "whoMythBustersListOfItemsPageListItem4" : MessageLookupByLibrary.simpleMessage("A koronavírus KÉPES meleg, párás környezetben is terjedni"),
+    "whoMythBustersListOfItemsPageListItem5" : MessageLookupByLibrary.simpleMessage("A koronavírus szúnyogcsípéssel NEM terjed"),
+    "whoMythBustersListOfItemsPageListItem6" : MessageLookupByLibrary.simpleMessage("Arra NINCS bizonyíték, hogy állatok/házikedvencek (mint kutya, macska) képesek terjeszteni a fertőzést"),
+    "whoMythBustersListOfItemsPageListItem7" : MessageLookupByLibrary.simpleMessage("A forró fürdő NEM véd a koronavírus ellen"),
+    "whoMythBustersListOfItemsPageListItem8" : MessageLookupByLibrary.simpleMessage("A kézszárító NEM pusztítja el a koronavírust"),
+    "whoMythBustersListOfItemsPageListItem9" : MessageLookupByLibrary.simpleMessage("Ultraibolya fényt NEM SZABAD fertőtlenítéshez használni és bőrirritációt okozhat")
+  };
+}
