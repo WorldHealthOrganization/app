f1cb475bc89521016afa9726942f0b80064120b9
Ayush Ranjan <ayushr2@illinois.edu>
2020-04-01 13:29:24 -0500

    :white_check_mark: test(server): add app store servlet test
    Addresses #731

--- server/appengine/src/main/java/who/AppStoreServlet.java
+++ server/appengine/src/main/java/who/AppStoreServlet.java
@@ -1,9 +1,9 @@
 package who;
 
-import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Redirects to appropriate app store.
@@ -11,13 +11,15 @@ import javax.servlet.http.HttpServletResponse;
  * @author Bob Lee
  */
 public class AppStoreServlet extends HttpServlet {
+  static final String GOOGLE_PLAY_STORE_LINK = "https://play.google.com/store/apps/details?id=org.who.WHOMyHealth";
+  static final String APPLE_APP_STORE_LINK = "https://apps.apple.com/app/id1503458183";
 
   @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+          throws IOException {
     if (request.getHeader("User-Agent").toLowerCase().contains("android")) {
-      response.sendRedirect("https://play.google.com/store/apps/details?id=org.who.WHOMyHealth");
+      response.sendRedirect(GOOGLE_PLAY_STORE_LINK);
     } else {
-      response.sendRedirect("https://apps.apple.com/app/id1503458183");
+      response.sendRedirect(APPLE_APP_STORE_LINK);
     }
   }
 }
