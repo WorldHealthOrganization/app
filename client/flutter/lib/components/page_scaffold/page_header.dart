54a75527138eb7ef4dc19bd08d018cc7e2cd763a
Benjamin Swerdlow <swerdlowbenjamin@gmail.com>
2020-03-31 23:58:42 -0700

    Unpin header

--- client/flutter/lib/components/page_scaffold/page_header.dart
+++ client/flutter/lib/components/page_scaffold/page_header.dart
@@ -19,46 +19,14 @@ class PageHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverPersistentHeader(
-        pinned: true,
-        delegate: HeaderDelagate(
-            title: this.title,
-            subtitle: this.subtitle,
-            padding: this.padding,
-            showBackButton: this.showBackButton,
-            showLogo: this.showLogo));
-  }
-}
-
-class HeaderDelagate extends SliverPersistentHeaderDelegate {
-
-  final double maxHeight = 120.0;
-  final double minHeight = 80.0;
-
-  final String title;
-  final String subtitle;
-
-  final EdgeInsets padding;
-  final bool showBackButton;
-  final bool showLogo;
-
-  HeaderDelagate({
-    @required this.title,
-    @required this.subtitle,
-    @required this.padding,
-    @required this.showBackButton,
-    @required this.showLogo,
-  });
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return LayoutBuilder(builder: (context, constraints) {
-      final double currentHeight = constraints.maxHeight;
-      return _buildHeader(currentHeight);
-    });
+    return SliverAppBar(
+      expandedHeight: 120,
+      backgroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(background:_buildHeader()),
+    );
   }
 
-  Widget _buildHeader(double currentHeight) {
+  Widget _buildHeader() {
     List<Widget> headerItems = [
       if (this.showBackButton) BackArrow(),
       
@@ -71,7 +39,7 @@ class HeaderDelagate extends SliverPersistentHeaderDelegate {
               style: TextStyle(
                   color: Colors.blueAccent, fontWeight: FontWeight.bold)),
           SizedBox(height: 4),
-          if(this.minHeight< currentHeight-20) Text(this.subtitle,
+          Text(this.subtitle,
               textScaleFactor: 1.0,
               style:
                   TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
@@ -80,7 +48,7 @@ class HeaderDelagate extends SliverPersistentHeaderDelegate {
       Expanded(
         child: Container(),
       ),
-      showLogo && this.minHeight<currentHeight -20 ? Image.asset('assets/images/mark.png', width: 75) : Container(),
+      if(this.showLogo)Image.asset('assets/images/mark.png', width: 75)
     ];
     return Container(
       color: Colors.white,
@@ -96,12 +64,4 @@ class HeaderDelagate extends SliverPersistentHeaderDelegate {
     );
   }
 
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate _) => true;
-
-  @override
-  double get maxExtent => this.maxHeight;
-
-  @override
-  double get minExtent => this.minHeight;
 }
