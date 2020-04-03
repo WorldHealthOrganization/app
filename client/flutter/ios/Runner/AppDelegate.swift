9e8aa6a7f615970a9bf81dec9b8a343e0a3935b4
Dan Field <dnfield@google.com>
2020-04-02 20:25:16 -0700

    Fix late locale initialization on iOS (#831)

--- client/flutter/ios/Runner/AppDelegate.swift
+++ client/flutter/ios/Runner/AppDelegate.swift
@@ -8,11 +8,22 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    
+    // Work-around for https://github.com/flutter/flutter/issues/39032
+    // This is safe to remove once the fix for that is available in the SDK
+    // used by this application.
+    guard let controller = window?.rootViewController as? FlutterViewController else {
+      fatalError("rootViewController is not type FlutterViewController")
+    }
+    let engine = controller.engine!
+    let currentLocale = NSLocale.current
+    if (currentLocale.languageCode != nil) {
+      engine.localizationChannel?.invokeMethod("setLocale", arguments: [currentLocale.languageCode!, currentLocale.regionCode ?? "", currentLocale.scriptCode ?? "", currentLocale.variantCode ?? ""])
+    }
+
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
-    
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
