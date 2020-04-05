ddd2c060728a41eb30158b0afa5dc5e2768ea9ce
Britannio Jarrett <33752528+britannio@users.noreply.github.com>
2020-04-05 22:41:02 +0100

    P0 fix: add MediaQuery.textScaleFactorOf to page button text scale factor (#826)
    * fix: add MediaQuery.textScaleFactorOf to page button text scale factor
    
    * fix: remove textScaleFactor overrides on PermissionRequestPage

--- client/flutter/lib/components/page_button.dart
+++ client/flutter/lib/components/page_button.dart
@@ -47,7 +47,8 @@ class PageButton extends StatelessWidget {
             children: <Widget>[
               Text(
                 this.title,
-                textScaleFactor: 1.0 + 1.0 * contentScale(context),
+                // textScaleFactorOf is for the device font size
+                textScaleFactor: 1.0 + contentScale(context) * MediaQuery.textScaleFactorOf(context),
                 textAlign: TextAlign.left,
                 style: titleStyle ?? TextStyle(fontWeight: FontWeight.w700),
               ),
@@ -57,7 +58,7 @@ class PageButton extends StatelessWidget {
                   ? Text(
                       this.description,
                       textAlign: TextAlign.left,
-                      textScaleFactor: 0.9 + 0.5 * contentScale(context),
+                      textScaleFactor: (0.9 + 0.5 * contentScale(context)) * MediaQuery.textScaleFactorOf(context),
                       style: TextStyle(fontWeight: FontWeight.w400, color: descriptionColor ?? Color(0xFFC9CDD6)),
                     )
                   : Container()
