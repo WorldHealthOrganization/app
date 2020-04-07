8d661cfd84d9ff0524e4779f519814cb9b2d5bbc
Advay Mengle <source@madvay.com>
2020-04-07 13:24:02 -0700

    [feat] Store jurisdiction of client; closes #890

--- server/appengine/src/main/java/who/WhoServiceImpl.java
+++ server/appengine/src/main/java/who/WhoServiceImpl.java
@@ -1,5 +1,6 @@
 package who;
 
+import com.google.common.base.Strings;
 import com.google.common.geometry.S2CellId;
 import com.google.common.geometry.S2LatLng;
 import java.io.IOException;
@@ -19,6 +20,13 @@ public class WhoServiceImpl implements WhoService {
     Client client = Client.current();
     client.latitude = request.latitude;
     client.longitude = request.longitude;
+    client.countryCode = Strings.emptyToNull(request.countryCode);
+    client.adminArea1 = Strings.emptyToNull(request.adminArea1);
+    client.adminArea2 = Strings.emptyToNull(request.adminArea2);
+    client.adminArea3 = Strings.emptyToNull(request.adminArea3);
+    client.adminArea4 = Strings.emptyToNull(request.adminArea4);
+    client.adminArea5 = Strings.emptyToNull(request.adminArea5);
+    client.locality = Strings.emptyToNull(request.locality);
     S2LatLng coordinates = S2LatLng.fromDegrees(request.latitude, request.longitude);
     client.location = S2CellId.fromLatLng(coordinates).id();
     ofy().save().entities(client);
