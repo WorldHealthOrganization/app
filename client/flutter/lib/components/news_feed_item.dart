51026ad8df9cabf17cb7619285a829901e43a1a4
Britannio Jarrett <33752528+britannio@users.noreply.github.com>
2020-04-07 16:45:00 +0100

    fix: Realign news feed headings to start/end (#929)

--- client/flutter/lib/components/news_feed_item.dart
+++ client/flutter/lib/components/news_feed_item.dart
@@ -20,6 +20,7 @@ class NewsFeedItem extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
               this.title,
