b1a0fff4556e4da7e0a9a97242c832d01da0b61e
Britannio Jarrett <33752528+britannio@users.noreply.github.com>
2020-04-08 02:18:33 +0100

    fix: add semantics to settings page analytics switch (#933)

--- client/flutter/lib/pages/settings_page.dart
+++ client/flutter/lib/pages/settings_page.dart
@@ -17,73 +17,95 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool _switchValue;
+  bool _analyticsEnabled;
   int indexSelected = 0;
 
   @override
   void initState() {
     super.initState();
-    if (_switchValue == null) {
-      _initSwitch();
+    if (_analyticsEnabled == null) {
+      _initAnalyticsSwitch();
     }
   }
 
-  _initSwitch() async {
-    _switchValue = await UserPreferences().getAnalyticsEnabled();
+  _initAnalyticsSwitch() async {
+    _analyticsEnabled = await UserPreferences().getAnalyticsEnabled();
     setState(() {});
   }
 
   _toggleAnalytics(bool value) async {
     await UserPreferences().setAnalyticsEnabled(value);
+    setState(() {
+      _analyticsEnabled = value;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return PageScaffold(
       context,
-      body:[SliverList(
-        delegate:SliverChildListDelegate(
-      [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              ListTile(
-                leading: Text(
-                  S.of(context).homePagePageSliverListSettingsHeader1,
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.w700,
+      body: [
+        SliverList(
+            delegate: SliverChildListDelegate(
+          [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Semantics(
+                  toggled: _analyticsEnabled,
+                  child: InkWell(
+                    onTap: () => _toggleAnalytics(!_analyticsEnabled),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 16,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Text(
+                                  S
+                                      .of(context)
+                                      .homePagePageSliverListSettingsHeader1,
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                    fontSize: 25.0,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              Semantics(
+                                excludeSemantics: true,
+                                child: CupertinoSwitch(
+                                  activeColor: Constants.primaryColor,
+                                  value: _analyticsEnabled ?? false,
+                                  onChanged: _toggleAnalytics,
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            S
+                                .of(context)
+                                .homePagePageSliverListSettingsDataCollection,
+                            style: Theme.of(context).textTheme.subhead,
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                trailing: CupertinoSwitch(
-                  activeColor: Constants.primaryColor,
-                  value: _switchValue ?? false,
-                  onChanged: (newValue) async {
-                    await _toggleAnalytics(newValue);
-                    setState(() {
-                      _switchValue = newValue;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-                child: Text(
-                  S.of(context).homePagePageSliverListSettingsDataCollection,
-                  style: Theme.of(context).textTheme.subhead,
-                ),
-              ),
 
-              /// TODO: Implement UI:-
-              /// TODO:   selection of language preferences already created PR for it (#654)
-            ],
-          ),
-        ),
+                /// TODO: Implement UI:-
+                /// TODO:   selection of language preferences already created PR for it (#654)
+              ],
+            ),
+          ],
+        ))
       ],
-        ))],
       title: S.of(context).homePagePageSliverListSettings,
     );
   }
