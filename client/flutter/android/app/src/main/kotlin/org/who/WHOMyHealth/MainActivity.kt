0f0ce4eabb4b1d3ccf63f2d768d121948c40f278
Britannio <britanniojarrett@gmail.com>
2020-04-10 12:59:14 +0100

    refactor: use flutter_native_splash for splash screens

--- client/flutter/android/app/src/main/kotlin/org/who/WHOMyHealth/MainActivity.kt
+++ client/flutter/android/app/src/main/kotlin/org/who/WHOMyHealth/MainActivity.kt
@@ -4,9 +4,31 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    val flutter_native_splash = true
+    var originalStatusBarColor = 0
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        originalStatusBarColor = window.statusBarColor
+        window.statusBarColor = 0xffeaeaea.toInt()
+    }
+    val originalStatusBarColorFinal = originalStatusBarColor
+
         GeneratedPluginRegistrant.registerWith(this)
+    val vto = flutterView.viewTreeObserver
+    vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+      override fun onGlobalLayout() {
+        flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          window.statusBarColor = originalStatusBarColorFinal
+        }
+      }
+    })
+
     }
 }
