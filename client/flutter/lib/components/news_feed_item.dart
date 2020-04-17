de57fd04ef94240678872c456a97c54a06f0d90d
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2020-04-16 18:12:30 -0700

    Clean code
    - got rid of unnecessary material widget
    - added trailing commas to match flutter styling
    - ran flutter format

--- client/flutter/lib/components/news_feed_item.dart
+++ client/flutter/lib/components/news_feed_item.dart
@@ -7,53 +7,60 @@ class NewsFeedItem extends StatelessWidget {
   final ImageProvider imageProvider;
   final String url;
 
-  NewsFeedItem(
-      {@required this.title,
-      @required this.description,
-      @required this.imageProvider,
-      @required this.url});
+  NewsFeedItem({
+    @required this.title,
+    @required this.description,
+    @required this.imageProvider,
+    @required this.url,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      type:MaterialType.transparency,
-      child: InkWell(
-        onTap: () => launch(this.url),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                this.title,
-                textAlign: TextAlign.start,
-                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 28),
+    return InkWell(
+      onTap: () => launch(this.url),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              this.title,
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 28,
               ),
-              SizedBox(height: 14),
-              Row(
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image(
-                      height: 109,
-                      width: 109,
-                      image: this.imageProvider,
-                    ),
+            ),
+            SizedBox(height: 14),
+            Row(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image(
+                    height: 109,
+                    width: 109,
+                    image: this.imageProvider,
                   ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Flexible(
-                      child: Text(
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  child: Text(
                     this.description,
                     style: TextStyle(fontSize: 18),
-                  )),
-                  Center(child: Icon(Icons.arrow_forward_ios, color: Color(0xffC9CDD6),)),
-                ],
-              ),
-              Divider()
-            ],
-          ),
+                  ),
+                ),
+                Center(
+                  child: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Color(0xffC9CDD6),
+                  ),
+                ),
+              ],
+            ),
+            Divider(),
+          ],
         ),
       ),
     );
