52e9e58f73cfa83ebb5b79ed3fa19200924900c1
Advay Mengle <source@madvay.com>
2020-04-18 23:43:04 -0700

    license format

--- client/flutter/lib/main.dart
+++ client/flutter/lib/main.dart
@@ -78,7 +78,6 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-    _registerLicenses();
 
     // onMessage: Fires when app is foreground
     // onLaunch: Fires when user taps and app is in background.
@@ -113,29 +112,6 @@ class _MyAppState extends State<MyApp> {
         await UserPreferences().setFCMToken(token);
       }
     }
-  }
-
-  Future<LicenseEntry> _loadLicense() async {
-    final licenseText = await rootBundle.loadString('assets/REPO_LICENSE');
-    return LicenseEntryWithLineBreaks(
-        ["https://github.com/WorldHealthOrganization/app"], licenseText);
-  }
-
-  Future<LicenseEntry> _load3pLicense() async {
-    final licenseText =
-        await rootBundle.loadString('assets/THIRD_PARTY_LICENSE');
-    return LicenseEntryWithLineBreaks([
-      "https://github.com/WorldHealthOrganization/app - THIRD_PARTY_LICENSE"
-    ], licenseText);
-  }
-
-  _registerLicenses() {
-    LicenseRegistry.addLicense(() {
-      return Stream<LicenseEntry>.fromFutures(<Future<LicenseEntry>>[
-        _loadLicense(),
-        _load3pLicense(),
-      ]);
-    });
   }
 
   // TODO: Issue #902 This is not essential for basic operation but we should implement
