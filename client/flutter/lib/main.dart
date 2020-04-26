7abe5db7a70e7ff79c7dda333160306e642be4da
Ben <swerdlowbenjamin>
2020-04-25 19:41:36 -0700

    remove from content loading and undo debug stuff

--- client/flutter/lib/main.dart
+++ client/flutter/lib/main.dart
@@ -33,7 +33,7 @@ void main() async {
   final bool onboardingComplete =
       await UserPreferences().getOnboardingCompleted();
 
-  if (!onboardingComplete) {
+  if (onboardingComplete) {
     // Set `enableInDevMode` to true to see reports while in debug mode
     // This is only to be used for confirming that reports are being
     // submitted as expected. It is not intended to be used for everyday
