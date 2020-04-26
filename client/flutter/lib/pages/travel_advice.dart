162798e9420d246bd3cc859eb93fb2216d70dc63
Advay Mengle <source@madvay.com>
2020-04-26 15:45:12 -0700

    Improve travel advice page; closes #1128 (#1129)
    * Improve travel advice page; closes #1128
    
    * Fix insets

--- client/flutter/lib/pages/travel_advice.dart
+++ client/flutter/lib/pages/travel_advice.dart
@@ -1,12 +1,11 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:who_app/api/content/schema/advice_content.dart';
 import 'package:who_app/components/dialogs.dart';
-import 'package:who_app/components/page_button.dart';
 import 'package:who_app/components/page_scaffold/page_scaffold.dart';
+import 'package:who_app/constants.dart';
 import 'package:who_app/generated/l10n.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_html/flutter_html.dart';
 
 class TravelAdvice extends StatefulWidget {
   final AdviceDataSource dataSource;
@@ -56,53 +55,48 @@ class _TravelAdviceState extends State<TravelAdvice> {
     return SliverList(
         delegate: SliverChildListDelegate([
       Container(
-        color: Color(0xffD82037),
-        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-        child: Html(
-          data: _adviceContent?.banner ?? "",
-          defaultTextStyle: TextStyle(
-            color: Colors.white,
-            // The Html widget isn't using the textScaleFactor so here's
-            // the next best place to incorporate it
-            fontSize: 18 * MediaQuery.textScaleFactorOf(context),
-            height: 1.2,
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-        child: Semantics(
-          header: true,
-          child: Text(
-            S.of(context).travelAdvicePageListTitle,
-            style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w800,
-                color: Color(0xff050C1D)),
-          ),
-        ),
-      ),
+          padding: const EdgeInsets.all(28.0),
+          child: Column(children: <Widget>[
+            if (_adviceContent?.banner != null)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FaIcon(
+                    FontAwesomeIcons.exclamationTriangle,
+                    color: Constants.emergencyRed,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    _adviceContent.banner,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Constants.emergencyRed,
+                      height: 1.37,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            if (_adviceContent?.body != null)
+              Text(
+                _adviceContent.body,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Constants.textColor,
+                  height: 1.37,
+                ),
+              ),
+          ])),
       ..._getItems(context),
       SizedBox(
-        height: 30,
+        height: 28,
       ),
-      if (_adviceContent?.recommendationsLink != null) _buildLinkButton(context)
     ]));
-  }
-
-  Padding _buildLinkButton(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
-      child: PageButton(
-        Color(0xff008DC9),
-        S.of(context).travelAdvicePageButtonGeneralRecommendations,
-        () => launch(_adviceContent.recommendationsLink),
-        description: _adviceContent?.recommendations ?? "",
-        verticalPadding: 16,
-        titleStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        descriptionColor: Colors.white,
-      ),
-    );
   }
 
   SliverToBoxAdapter _buildLoading() {
@@ -116,40 +110,51 @@ class _TravelAdviceState extends State<TravelAdvice> {
   List<Widget> _getItems(BuildContext context) {
     return (_adviceContent?.items ?? []).map((item) {
       return TravelAdviceListItem(
-          imageSrc: 'assets/travel_advice/${item.imageName}.png',
-          description: item.body ?? "");
+          title: item.title ?? "", description: item.body ?? "");
     }).toList();
   }
 }
 
 class TravelAdviceListItem extends StatelessWidget {
+  final String title;
   final String description;
-  final String imageSrc;
 
-  TravelAdviceListItem({@required this.description, @required this.imageSrc});
+  TravelAdviceListItem({@required this.title, @required this.description});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(28.0),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image.asset(
-              this.imageSrc,
-              height: 109,
-              width: 109,
-            ),
+          FaIcon(
+            FontAwesomeIcons.solidCheckCircle,
+            color: Constants.primaryColor,
           ),
           SizedBox(
-            width: 16,
+            width: 12,
           ),
           Flexible(
-              child: Text(
-            this.description,
-            style: TextStyle(fontSize: 18, color: Color(0xff3C4245)),
-          )),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                Text(
+                  this.title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      height: 1.37,
+                      color: Constants.bodyTextColor),
+                ),
+                Text(
+                  this.description,
+                  style: TextStyle(
+                      fontSize: 18,
+                      height: 1.37,
+                      color: Constants.bodyTextColor),
+                ),
+              ])),
         ],
       ),
     );
