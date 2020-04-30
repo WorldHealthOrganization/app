bc0372b794bfc26a0c2e085c32602ba944db1416
Hunter Spinks <hspinks@gmail.com>
2020-04-29 20:33:24 -0700

    :sparkles: feat: create HomePageInformationCard widget UI

--- client/flutter/lib/pages/main_pages/home_page.dart
+++ client/flutter/lib/pages/main_pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:who_app/api/linking.dart';
 import 'package:who_app/components/home_page_header.dart';
+import 'package:who_app/components/home_page_information_card.dart';
 import 'package:who_app/components/home_page_recent_numbers.dart';
 import 'package:who_app/components/page_scaffold/page_scaffold.dart';
 import 'package:who_app/constants.dart';
@@ -13,6 +15,19 @@ class HomePage extends StatelessWidget {
       body: [
         HomePageHeader(HeaderType.ProtectYourself),
         HomePageRecentNumbers(),
+        SliverToBoxAdapter(
+          // TODO: drive this dynamically
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: HomePageInformationCard(
+              buttonText: 'Learn more',
+              link: RouteLink.fromUri('/get-the-facts'),
+              subtitle:
+                  'Spraying alcohol or chlorine all over your body does not kill the new coronavirus.',
+              title: 'Get the Facts',
+            ),
+          ),
+        )
       ],
     );
   }
