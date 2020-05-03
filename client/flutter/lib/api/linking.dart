068b7a7b10f21a7933ed7377a8e33e54669f81d8
Hunter Spinks <hspinks@gmail.com>
2020-05-03 15:29:05 -0700

    :ok_hand: feat: revert to preserving core constructor to code review feedback

--- client/flutter/lib/api/linking.dart
+++ client/flutter/lib/api/linking.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class RouteLink {
   Uri _url;
+  String route;
+  Map<String, String> args;
 
-  String get route => _url.path;
-  Map<String, String> get args => _url.queryParameters ?? {};
   bool get isExternal {
     try {
-      return _url.origin != null;
+      return _url?.origin != null;
     } catch (error) {
       return false;
     }
   }
 
-  String get url => _url.toString();
+  String get url => _url?.toString();
 
-  RouteLink();
+  RouteLink({
+    @required this.route,
+    @required this.args,
+  });
+
+  Future open(BuildContext context) {
+    return this.isExternal
+        ? launch(this.url)
+        : Navigator.of(context, rootNavigator: true)
+            .pushNamed(this.route, arguments: this.args);
+  }
 
   RouteLink.fromUri(String uri) {
     _url = Uri.parse(uri);
+    route = _url.path;
+    args = _url.queryParameters ?? {};
   }
 }
