8b4241f6c8d179d9272bb87de1e684579c6e6a6f
Mark Graves <mark@myire.com>
2020-05-07 18:33:10 -0700

    refactor(case-stats): Daily case stats come from WHO ARCGIS endpoint

--- server/appengine/src/main/java/who/RefreshCaseStatsServlet.java
+++ server/appengine/src/main/java/who/RefreshCaseStatsServlet.java
@@ -17,7 +17,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class RefreshCaseStatsServlet extends HttpServlet {
-  private static final String WHO_CASE_STATS_URL = "https://dashboards-dev.sprinklr.com/data/9043/global-covid19-who-gis.json";
+  private static final String WHO_CASE_STATS_URL = "https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/COVID19_hist_cases_adm0_v5_view/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=ISO_2_CODE%2Cdate_epicrv%2CNewCase%2CCumCase%2CNewDeath%2CCumDeath%2C+ADM0_NAME&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=";
 
   private static final OkHttpClient HTTP_CLIENT = new OkHttpClient();
 
@@ -41,18 +41,18 @@ public class RefreshCaseStatsServlet extends HttpServlet {
 
     String jsonString = getDashboardContents();
     JsonObject root = new JsonParser().parse(jsonString).getAsJsonObject();
-    JsonArray rows = root.getAsJsonArray("rows");
+    JsonArray rows = root.getAsJsonArray("features");
     long globalTotalCases = 0L, globalTotalDeaths = 0L;
     Map<Long, StoredCaseStats.StoredStatSnapshot> globalSnapshots = new HashMap<>();
     // Given that each row has heterogeneous elements, not sure there is much benefit
     // to using gson with reflection here.
-    for (JsonElement country : rows) {
-      JsonArray row = country.getAsJsonArray();
-      long timestamp = row.get(0).getAsLong();
-      long dailyDeaths = row.get(3).getAsLong();
-      long totalDeaths = row.get(4).getAsLong();
-      long dailyCases = row.get(5).getAsLong();
-      long totalCases = row.get(6).getAsLong();
+    for (JsonElement feature : rows) {
+      JsonObject attributes = feature.getAsJsonObject();
+      long timestamp = attributes.get("date_epicrv").getAsLong();
+      long dailyDeaths = attributes.get("NewDeath").getAsLong();
+      long totalDeaths = attributes.get("CumDeath").getAsLong();
+      long dailyCases = attributes.get("NewCase").getAsLong();
+      long totalCases = attributes.get("CumCase").getAsLong();
       globalTotalCases += dailyCases;
       globalTotalDeaths += dailyDeaths;
 
