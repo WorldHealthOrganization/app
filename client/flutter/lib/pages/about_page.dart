9d14826491194b2849e43e3e6c69601af5bba3d1
Britannio Jarrett <33752528+britannio@users.noreply.github.com>
2020-05-12 17:02:02 +0100

    refactor: use MenuListTile in AboutPage (#1259)
    * use MenuListTile in AboutPage
    
    * use async await when opening links/pages in about page
    
    * increase padding above copyright text

--- client/flutter/lib/pages/about_page.dart
+++ client/flutter/lib/pages/about_page.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:who_app/components/menu_list_tile.dart';
 import 'package:who_app/components/page_scaffold/page_scaffold.dart';
 import 'package:who_app/components/themed_text.dart';
 import 'package:who_app/generated/l10n.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_html/rich_text_parser.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:who_app/main.dart';
 import 'package:who_app/pages/license_page.dart' as who;
@@ -23,64 +23,56 @@ class AboutPage extends StatelessWidget {
         .of(context)
         .commonWorldHealthOrganizationCoronavirusCopyright(DateTime.now().year);
 
+    Future<void> _openTermsOfService(BuildContext context) async {
+      final String url = S.of(context).aboutPageTermsOfServiceLinkUrl;
+      await launch(url);
+      await FirebaseAnalytics().logEvent(name: 'TermsOfService');
+    }
+
+    Future<void> _openPrivacyPolicy(BuildContext context) async {
+      final String url = S.of(context).legalLandingPagePrivacyPolicyLinkUrl;
+      await launch(url);
+      await FirebaseAnalytics().logEvent(name: 'PrivacyPolicy');
+    }
+
+    Future<void> _openLicenses(BuildContext context) async {
+      // Logs the page switch as it happens while avoiding any delay caused
+      // by logging.
+      // ignore: unawaited_futures
+      FirebaseAnalytics().logEvent(name: 'ViewLicenses');
+      await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) => who.LicensePage()),
+      );
+    }
+
+    // TODO add divider theme to ThemeData once we've switched to a MaterialApp
+    Widget _divider() =>
+        Divider(height: 1, thickness: 1, color: Color(0xFFC9CDD6));
+
     return PageScaffold(
       body: [
         SliverList(
           delegate: SliverChildListDelegate.fixed(
             [
-              Container(
-                color: CupertinoColors.white,
-                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Text.rich(
-                  TextSpan(
-                    style: TextStyle(color: CupertinoColors.black),
-                    children: [
-                      LinkTextSpan(
-                          text: S.of(context).aboutPageTermsOfServiceLinkText,
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: CupertinoColors.activeBlue),
-                          url: S.of(context).aboutPageTermsOfServiceLinkUrl,
-                          onLinkTap: (v) {
-                            FirebaseAnalytics()
-                                .logEvent(name: 'TermsOfService');
-                            return launch(v);
-                          }),
-                      TextSpan(text: "  —  "),
-                      LinkTextSpan(
-                          text: S.of(context).aboutPagePrivacyPolicyLinkText,
-                          style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              color: CupertinoColors.activeBlue),
-                          url: S
-                              .of(context)
-                              .legalLandingPagePrivacyPolicyLinkUrl,
-                          onLinkTap: (v) {
-                            FirebaseAnalytics().logEvent(name: 'PrivacyPolicy');
-                            return launch(v);
-                          }),
-                      TextSpan(text: "  —  "),
-                      LinkTextSpan(
-                          text: S.of(context).aboutPageViewLicensesLinkText,
-                          style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: CupertinoColors.activeBlue,
-                          ),
-                          onLinkTap: (v) {
-                            FirebaseAnalytics().logEvent(name: 'ViewLicenses');
-                            return Navigator.of(context)
-                                .push(CupertinoPageRoute(
-                              builder: (c) => who.LicensePage(),
-                            ));
-                          }),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
+              MenuListTile(
+                title: S.of(context).aboutPageTermsOfServiceLinkText,
+                onTap: () => _openTermsOfService(context),
               ),
+              _divider(),
+              MenuListTile(
+                title: S.of(context).aboutPagePrivacyPolicyLinkText,
+                onTap: () => _openPrivacyPolicy(context),
+              ),
+              _divider(),
+              MenuListTile(
+                title: S.of(context).aboutPageViewLicensesLinkText,
+                onTap: () => _openLicenses(context),
+              ),
+              _divider(),
               Container(
                 color: CupertinoColors.white,
-                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                padding: EdgeInsets.fromLTRB(25, 25, 25, 10),
                 child: ThemedText(
                   S.of(context).aboutPageBuiltByCreditText(
                       copyrightString, versionString),
