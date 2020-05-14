7d96e07ec90563010395b1932d6fa29af3f2a021
Britannio <britanniojarrett@gmail.com>
2020-05-14 01:22:22 +0100

    use material design nav bar on android

--- client/flutter/lib/pages/main_pages/app_tab_router.dart
+++ client/flutter/lib/pages/main_pages/app_tab_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:who_app/api/content/schema/index_content.dart';
+import 'package:who_app/components/platform_widget.dart';
 import 'package:who_app/constants.dart';
 import 'package:who_app/pages/main_pages/check_up_intro_page.dart';
 import 'package:who_app/pages/main_pages/recent_numbers.dart';
@@ -8,7 +9,7 @@ import 'package:who_app/pages/main_pages/learn_page.dart';
 import 'package:who_app/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 
-class AppTabRouter extends StatelessWidget {
+class AppTabRouter extends StatefulWidget {
   static final List<Widget Function(BuildContext)> defaultTabs = [
     (context) => HomePage(
           dataSource: IndexContent.homeIndex,
@@ -41,6 +42,15 @@ class AppTabRouter extends StatelessWidget {
 
   AppTabRouter(this.tabs, this.navItems);
 
+  @override
+  _AppTabRouterState createState() => _AppTabRouterState();
+}
+
+class _AppTabRouterState extends State<AppTabRouter> {
+  int currentIndex = 0;
+  final Color selectedColor = Constants.accentColor;
+  final Color unselectedColor = Colors.black;
+
   Widget wrapTabView(Widget Function(BuildContext) builder) {
     return Material(
       child: CupertinoTabView(
@@ -49,19 +59,36 @@ class AppTabRouter extends StatelessWidget {
     );
   }
 
+  void _switchPage(int index) {
+    if (currentIndex != index) {
+      setState(() {
+        currentIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBuilder: (BuildContext context, int index) {
-        if (index < tabs.length) {
-          return wrapTabView(tabs[index]);
-        }
-        return null;
-      },
-      tabBar: CupertinoTabBar(
-        inactiveColor: CupertinoColors.black,
-        activeColor: Constants.accentColor,
-        items: navItems,
+    return Scaffold(
+      body: widget.tabs[currentIndex](context),
+      extendBody: Theme.of(context).platform == TargetPlatform.iOS,
+      bottomNavigationBar: PlatformWidget(
+        material: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: _switchPage,
+          items: widget.navItems,
+          selectedItemColor: selectedColor,
+          unselectedItemColor: unselectedColor,
+          type: BottomNavigationBarType.fixed,
+          selectedFontSize: 12,
+        ),
+        cupertino: CupertinoTabBar(
+          currentIndex: currentIndex,
+          onTap: _switchPage,
+          items: widget.navItems,
+          activeColor: selectedColor,
+          inactiveColor: unselectedColor,
+        ),
       ),
     );
   }
