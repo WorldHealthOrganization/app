d75daee69db78fe3af41bf93cdd1d7046f404d63
Hunter Spinks <hspinks@gmail.com>
2020-05-13 17:38:37 -0700

    :sparkles: feat: update news page to new design

--- client/flutter/lib/components/menu_list_tile.dart
+++ client/flutter/lib/components/menu_list_tile.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:who_app/components/themed_text.dart';
+import 'package:who_app/constants.dart';
 
 class MenuListTile extends StatelessWidget {
   const MenuListTile({
     Key key,
     @required this.title,
     @required this.onTap,
+    this.subtitle,
+    this.contentPadding =
+        const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
   }) : super(key: key);
 
   final String title;
+  final String subtitle;
+  final EdgeInsets contentPadding;
   final VoidCallback onTap;
 
   @override
@@ -16,9 +22,21 @@ class MenuListTile extends StatelessWidget {
     return Material(
       color: Colors.white,
       child: ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+        contentPadding: contentPadding,
         title: ThemedText(title, variant: TypographyVariant.button),
-        trailing: Icon(Icons.arrow_forward_ios, color: Color(0xFFC9CDD6)),
+        subtitle: subtitle != null
+            ? ThemedText(
+                subtitle,
+                variant: TypographyVariant.body,
+              )
+            : null,
+        isThreeLine: subtitle != null,
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.arrow_forward_ios, color: Constants.neutral3Color),
+          ],
+        ),
         onTap: onTap,
       ),
     );
