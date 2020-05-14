28ec39bf15cf4668672be91bba403e37ff6a1c60
Hunter Spinks <hspinks@gmail.com>
2020-05-13 23:16:44 -0700

    :lipstick: feat: update country select and notifications onboarding pages to new designs

--- client/flutter/lib/pages/onboarding/notifications_page.dart
+++ client/flutter/lib/pages/onboarding/notifications_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:who_app/api/notifications.dart';
+import 'package:who_app/components/button.dart';
+import 'package:who_app/components/themed_text.dart';
+import 'package:who_app/constants.dart';
 import 'package:who_app/generated/l10n.dart';
-import 'package:who_app/pages/onboarding/permission_request_page.dart';
 
 class NotificationsPage extends StatefulWidget {
   final VoidCallback onNext;
@@ -17,15 +21,93 @@ class _NotificationsPageState extends State<NotificationsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return PermissionRequestPage(
-      pageTitle: S.of(context).notificationsPagePermissionRequestPageTitle,
-      pageDescription:
-          S.of(context).notificationsPagePermissionRequestPageDescription,
-      buttonTitle: S.of(context).notificationsPagePermissionRequestPageButton,
-      backgroundImageSrc:
-          S.of(context).notificationsPagePermissionRequestBackgroundImage,
-      onGrantPermission: _allowNotifications,
-      onSkip: _skipNotifications,
+    return Material(
+      color: CupertinoColors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomLeft,
+              padding: EdgeInsets.only(left: 36.0, bottom: 18.0),
+              child: SvgPicture.asset(
+                  'assets/svg/undraw-onboarding-notifications.svg'),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 36.0, right: 20.0, bottom: 18.0),
+                child: ThemedText(
+                  S.of(context).notificationsPagePermissionRequestPageTitle,
+                  variant: TypographyVariant.h2,
+                  style: TextStyle(
+                    color: Constants.accentNavyColor,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 36.0, right: 54.0, bottom: 42.0),
+                child: ThemedText(
+                  S
+                      .of(context)
+                      .notificationsPagePermissionRequestPageDescription,
+                  variant: TypographyVariant.body,
+                  style: TextStyle(
+                    color: Constants.neutral1Color,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(24.0, 32.0, 24.0, 112.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Button(
+                      onPressed: () async {
+                        await _allowNotifications();
+                      },
+                      color: Constants.whoBackgroundBlueColor,
+                      borderRadius: BorderRadius.circular(50.0),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 32.0,
+                          vertical: 12.0,
+                        ),
+                        child: ThemedText(
+                          S
+                              .of(context)
+                              .notificationsPagePermissionRequestPageButton,
+                          variant: TypographyVariant.button,
+                          style: TextStyle(
+                            color: CupertinoColors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    CupertinoButton(
+                      onPressed: () async {
+                        await _skipNotifications();
+                      },
+                      child: ThemedText(
+                        S.of(context).commonPermissionRequestPageButtonSkip,
+                        variant: TypographyVariant.button,
+                        style: TextStyle(
+                          color:
+                              Constants.neutralTextLightColor.withOpacity(0.5),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 
