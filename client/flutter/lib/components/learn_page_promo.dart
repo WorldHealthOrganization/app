ca97217237fdb294153e48116555bd9e78fb7a57
onkar-shaligram <onkarshaligram01@gmail.com>
2020-06-15 02:47:52 +0530

    Notification bar problem solved

--- client/flutter/lib/components/learn_page_promo.dart
+++ client/flutter/lib/components/learn_page_promo.dart
@@ -28,67 +28,70 @@ class LearnPagePromo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: <Widget>[
-        Positioned.fill(
-          child: PromoCurvedBackground(
-            color: Constants.primaryDarkColor,
-          ),
-        ),
-        if (this.assetName != null)
+    return SafeArea(
+      top: true,
+          child: Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
           Positioned.fill(
-              child: FittedBox(child: SvgPicture.asset(this.assetName))),
-        SafeArea(
-            bottom: false,
-            child: Container(
-              child: Column(children: [
-                SizedBox(
-                  height: 24.0,
-                ),
-                ThemedText(
-                  title,
-                  variant: TypographyVariant.h3,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: CupertinoColors.white,
+            child: PromoCurvedBackground(
+              color: Constants.primaryDarkColor,
+            ),
+          ),
+          if (this.assetName != null)
+            Positioned.fill(
+                child: FittedBox(child: SvgPicture.asset(this.assetName))),
+          SafeArea(
+              bottom: false,
+              child: Container(
+                child: Column(children: [
+                  SizedBox(
+                    height: 24.0,
                   ),
-                ),
-                ThemedText(
-                  subtitle,
-                  variant: TypographyVariant.body,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: CupertinoColors.white,
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Button(
-                  color: CupertinoColors.white,
-                  borderRadius: BorderRadius.circular(50),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 32,
-                    vertical: 8,
-                  ),
-                  child: ThemedText(
-                    buttonText,
-                    variant: TypographyVariant.button,
+                  ThemedText(
+                    title,
+                    variant: TypographyVariant.h3,
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      color: Constants.primaryColor,
+                      color: CupertinoColors.white,
                     ),
                   ),
-                  onPressed: () {
-                    return this.link.open(context);
-                  },
-                ),
-                SizedBox(
-                  height: 80.0,
-                )
-              ]),
-            ))
-      ],
+                  ThemedText(
+                    subtitle,
+                    variant: TypographyVariant.body,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: CupertinoColors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Button(
+                    color: CupertinoColors.white,
+                    borderRadius: BorderRadius.circular(50),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 8,
+                    ),
+                    child: ThemedText(
+                      buttonText,
+                      variant: TypographyVariant.button,
+                      style: TextStyle(
+                        color: Constants.primaryColor,
+                      ),
+                    ),
+                    onPressed: () {
+                      return this.link.open(context);
+                    },
+                  ),
+                  SizedBox(
+                    height: 80.0,
+                  )
+                ]),
+              ))
+        ],
+      ),
     );
   }
 }
