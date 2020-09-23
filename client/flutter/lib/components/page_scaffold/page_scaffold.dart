327a6c195a58addffc5b6aa5c9d74c6d734afd94
Advay Mengle <source@madvay.com>
2020-09-23 09:25:45 -0700

    Approved by WHO Legal Joris Auert: Non-production server disclaimers (#1517)

--- client/flutter/lib/components/page_scaffold/page_scaffold.dart
+++ client/flutter/lib/components/page_scaffold/page_scaffold.dart
@@ -78,25 +78,62 @@ class PageScaffold extends StatelessWidget {
   }
 }
 
-class Alerts extends StatelessWidget {
-  const Alerts({
+class AlertsWrapper extends StatefulWidget {
+  const AlertsWrapper({
     Key key,
     @required this.alerts,
+    @required this.child,
   }) : super(key: key);
 
   final List<Alert> alerts;
+  final Widget child;
+
+  @override
+  _AlertsWrapperState createState() => _AlertsWrapperState(alerts);
+}
+
+abstract class AlertController {
+  void removeAlert(Alert a);
+}
+
+class _AlertsWrapperState extends State<AlertsWrapper>
+    implements AlertController {
+  _AlertsWrapperState(List<Alert> alerts) : _alerts = List.from(alerts);
+
+  final List<Alert> _alerts;
+
+  void removeAlert(Alert a) {
+    if (mounted) {
+      setState(() {
+        _alerts.remove(a);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: SafeArea(
-        bottom: false,
-        maintainBottomViewPadding: true,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: alerts.map((alert) => _Alert(alert: alert)).toList(),
-        ),
+    return Material(
+      child: Column(
+        children: [
+          if (_alerts.isNotEmpty)
+            Container(
+                color: Colors.red,
+                child: SafeArea(
+                  bottom: false,
+                  maintainBottomViewPadding: true,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: _alerts
+                        .map((alert) => _Alert(alert: alert, controller: this))
+                        .toList(),
+                  ),
+                )),
+          Expanded(
+              child: MediaQuery.removePadding(
+                  removeTop: _alerts.isNotEmpty,
+                  context: context,
+                  child: widget.child)),
+        ],
       ),
     );
   }
@@ -107,10 +144,12 @@ class _Alert extends StatelessWidget {
     Key key,
     this.topPadding = 0,
     @required this.alert,
+    @required this.controller,
   }) : super(key: key);
 
   final double topPadding;
   final Alert alert;
+  final AlertController controller;
 
   @override
   Widget build(BuildContext context) {
@@ -124,16 +163,39 @@ class _Alert extends StatelessWidget {
           SizedBox(
             height: topPadding,
           ),
-          ThemedText(
-            alert.title,
-            variant: TypographyVariant.headerSmall,
-            style: TextStyle(color: Colors.white),
-          ),
-          ThemedText(
-            alert.body,
-            variant: TypographyVariant.body,
-            style: TextStyle(color: Colors.white),
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  if (alert.title != null)
+                    ThemedText(
+                      alert.title,
+                      variant: TypographyVariant.headerSmall,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  if (alert.body != null)
+                    ThemedText(
+                      alert.body,
+                      variant: TypographyVariant.body,
+                      style: TextStyle(color: Colors.white, height: 1.2),
+                    ),
+                ],
+              )),
+              if (alert.dismissable)
+                IconButton(
+                    icon: Icon(Icons.close),
+                    color: Colors.white,
+                    onPressed: () {
+                      controller.removeAlert(alert);
+                    }),
+            ],
+          )
         ],
       ),
     );
