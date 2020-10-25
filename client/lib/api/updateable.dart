3f548bcb5215a72a739fa9c7054631092f3b5cd3
Bruno Bowden <github@brunobowden.com>
2020-10-25 16:04:28 -0700

    simple_connectivity v0.0.1 => connectivity v2.0.0
    - Closes #1532
    - Replace 3rd party simple_connectivity with Google connectivity package
    - New version removes dependency on locations permission:
    - compileSdkVersion 29 - matching targetSdk and Flutter packages
    - Java 11 for CI environment to fix build failure where `Werror` flag
    in connectivity causes `source value 7 is obsolete` warning to fail build.

--- client/lib/api/updateable.dart
+++ client/lib/api/updateable.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:simple_connectivity/simple_connectivity.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
