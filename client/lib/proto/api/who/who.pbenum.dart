029006ba94b19131882e5f981465bc7185a5e14d
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-11-13 11:17:31 -0500

    Fix CI by regenerating protobuf files (#1737)

--- client/lib/proto/api/who/who.pbenum.dart
+++ client/lib/proto/api/who/who.pbenum.dart
@@ -5,7 +5,7 @@
 // @dart = 2.3
 // ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
 
-// ignore_for_file: UNDEFINED_SHOWN_NAME,UNUSED_SHOWN_NAME
+// ignore_for_file: UNDEFINED_SHOWN_NAME
 import 'dart:core' as $core;
 import 'package:protobuf/protobuf.dart' as $pb;
 
