273d6d894d0e946536243f928c53dbfda15d4223
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-11-17 18:48:59 -0500

    Properly set firebase auto init option when notifications / analytics settings are changed (#1736)
    * Properly set firebase auto init option when notifications / analytics settings are changed
    
    * refactor

--- client/lib/api/user_preferences.dart
+++ client/lib/api/user_preferences.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:uuid/uuid.dart';
@@ -81,8 +82,10 @@ class UserPreferences {
       await analytics.resetAnalyticsData();
     }
     await analytics.setAnalyticsCollectionEnabled(value);
-    return (await SharedPreferences.getInstance())
+    var result = (await SharedPreferences.getInstance())
         .setBool(UserPreferenceKey.AnalyticsEnabled.toString(), value);
+    await _updateFirebaseAutoInit();
+    return result;
   }
 
   Future<bool> getNotificationsEnabled() async {
@@ -92,8 +95,21 @@ class UserPreferences {
   }
 
   Future<bool> setNotificationsEnabled(bool value) async {
-    return (await SharedPreferences.getInstance())
+    var result = (await SharedPreferences.getInstance())
         .setBool(UserPreferenceKey.NotificationsEnabled.toString(), value);
+    await _updateFirebaseAutoInit();
+    return result;
+  }
+
+  Future<void> _updateFirebaseAutoInit() async {
+    // Updates the firebase auto init option. This method is called
+    // after a user modifies either analytics or notifications.
+    var firebaseMessaging = FirebaseMessaging();
+
+    // Enable Firebase Auto Init iff either notifications or analytics is enabled
+    var firebase_auto_init =
+        (await getNotificationsEnabled()) || (await getAnalyticsEnabled());
+    await firebaseMessaging.setAutoInitEnabled(firebase_auto_init);
   }
 
   Future<String> getFirebaseToken() async {
