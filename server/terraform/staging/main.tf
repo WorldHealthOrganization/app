bf000d0ff5af8b01af7a46996b8bab101eb84913
Bruno Bowden <github@brunobowden.com>
2020-11-17 22:58:42 -0800

    who-mh-staging + robust bucket creation
    who-mh-staging as new project
    
    Logging depends_on for robust oucket creation
    - Bucket creation initially fails as project doesn't exist:
       - `Error: Error creating Bucket: googleapi: Error 404: Project does not exist: who-mh-staging`
    - Then Terraforms believes that bucket exists but fails trying to find it:
       - `Error: googleapi: Error 404: Bucket europe-west1-logs-bucket does not exist`

---
+++ server/terraform/staging/main.tf
@@ -0,0 +1,8 @@
+# hack - penetration testing and ethical hacking, no privacy expectation
+
+module "myhealth" {
+  source     = "../modules/myhealth"
+  project_id = "who-mh-staging"
+  # TODO: migrate to staging.whocoronavirus.org
+  domain = "staging-temp.whocoronavirus.org"
+}
