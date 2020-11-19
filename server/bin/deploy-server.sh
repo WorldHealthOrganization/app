fa4a6b7ca1d56f23697f2a2244f1b5dcd6f08fb8
Bruno Bowden <github@brunobowden.com>
2020-11-18 17:01:09 -0800

    Load Balancer for all App Engine traffic (#1756)
    - App Engine ingress set to `internal-and-cloud-load-balancing`
    - Disables who-mh-hack.appspot.com domain
    - Partial fix for #649

--- server/bin/deploy-server.sh
+++ server/bin/deploy-server.sh
@@ -8,5 +8,9 @@ PROJECT=$1
 ../tools/verify-server.sh
 
 ./gradlew build
-gcloud beta app deploy --quiet --project=$PROJECT appengine/build/war
-gcloud beta app deploy --quiet --project=$PROJECT appengine/build/war/WEB-INF/cron.yaml
+gcloud beta app --quiet --project=$PROJECT deploy appengine/build/war
+gcloud beta app --quiet --project=$PROJECT deploy appengine/build/war/WEB-INF/cron.yaml
+
+# Force everything through the load balancer for security
+gcloud beta app --quiet --project=$PROJECT services update default \
+  --ingress internal-and-cloud-load-balancing
