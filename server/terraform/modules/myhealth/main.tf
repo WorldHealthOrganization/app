23d4adc1ecd961d373c6cba0fe18cc3c8a3b197c
matthewblain <mb7oo@hotmail.com>
2020-11-21 19:26:27 -0800

    Create placeholder IP blocking security policy (#1762)
    * Create placeholder IP blocking security policy
    
    Create a placeholder Google Cloud Armor security policy blocking IP addresses.
    Uses a loopback placeholder with a CIDR and IP example.
    
    To edit interactively, view https://console.cloud.google.com/net-security/securitypolicies/lb-security-policy/rules/1000/edit
    Then be sure to update Terraform config, as it will overwrite on subsequent apply.
    
    Fixes #1758

--- server/terraform/modules/myhealth/main.tf
+++ server/terraform/modules/myhealth/main.tf
@@ -190,6 +190,54 @@ resource "google_compute_region_network_endpoint_group" "neg" {
   depends_on = [google_project_service.service]
 }
 
+
+# Google Cloud Armor security policy.
+resource "google_compute_security_policy" "policy" {
+  name = "lb-security-policy"
+
+  # IP Block list. 
+  # Up to ten IP ranges can be created per rule.
+  # Each rule is identified by its priority, from 1=highest to maxint32=default.
+  rule {
+    action   = "deny(403)"
+    priority = "1000"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        # Using an unused portion of the loopback block as a placeholder.
+        # Remove it once a real set of IP Ranges are present.
+        src_ip_ranges = ["127.0.0.255/32", "127.0.0.254"]
+      }
+    }
+    description = "Deny access to specified IPs"
+  }
+
+  # The default rule at maxint32. Should not need to be changed.
+  rule {
+    action   = "allow"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default rule"
+  }
+
+  # In case this resource is renamed in Terraform, the lifecycle must be:
+  #  1: Create new policy 
+  #  2: Configure backend to reference it
+  #  3: Delete old policy.
+  # To accomplish this, it is set to create_before_destroy, 
+  # then running apply twice completes the operation.
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
 resource "google_compute_backend_service" "backend" {
   name                            = "${var.project_id}-backend-appengine"
   load_balancing_scheme           = "EXTERNAL"
@@ -200,6 +248,7 @@ resource "google_compute_backend_service" "backend" {
   cdn_policy {
     signed_url_cache_max_age_sec = 3600
   }
+  security_policy = google_compute_security_policy.policy.id
   backend {
     capacity_scaler = 1
     group           = google_compute_region_network_endpoint_group.neg.id
