8432547861704ac44eda69fb93cb74ad26532768
matthewblain <mb7oo@hotmail.com>
2020-11-25 19:35:12 -0800

    Update deploy scripts

--- tools/deploy-production-static-serving.sh
+++ tools/deploy-production-static-serving.sh
@@ -3,4 +3,8 @@
 set -euv
 cd $(dirname "$0")
 
+# TODO: Once client has moved to new bucket, stop pushing to old bucket.
+# Old bucket name.
 GS_BUCKET=who-myhealth-europe-static-content-01 ./build-and-push-static-serving.sh
+# New bucket name.
+GS_BUCKET=who-mh-prod-static-content ./build-and-push-static-serving.sh
