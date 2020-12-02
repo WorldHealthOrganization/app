e811412308e90dc616ac8c155b27e2d75723946c
marcmcl <41429145+marcmcl@users.noreply.github.com>
2020-12-02 15:01:40 +0000

    - Various fixes to pass pre-commit checks

--- client/packages/get_proxy/android/src/main/java/net/marcs/get_proxy/GetProxyPlugin.java
+++ client/packages/get_proxy/android/src/main/java/net/marcs/get_proxy/GetProxyPlugin.java
@@ -1,10 +1,9 @@
 package net.marcs.get_proxy;
 
-import androidx.annotation.NonNull;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.ProxyInfo;
-
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -14,32 +13,36 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** GetProxyPlugin */
 public class GetProxyPlugin implements FlutterPlugin, MethodCallHandler {
+
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private ConnectivityManager manager;
-  
+
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "get_proxy");
+  public void onAttachedToEngine(
+    @NonNull FlutterPluginBinding flutterPluginBinding
+  ) {
+    channel =
+      new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "get_proxy");
     channel.setMethodCallHandler(this);
-    manager = (ConnectivityManager) flutterPluginBinding.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+    manager =
+      (ConnectivityManager) flutterPluginBinding
+        .getApplicationContext()
+        .getSystemService(Context.CONNECTIVITY_SERVICE);
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getProxy")) {
-      if(manager != null)
-      {
+      if (manager != null) {
         ProxyInfo proxyInfo = manager.getDefaultProxy();
-        if(proxyInfo != null)
-        {
+        if (proxyInfo != null) {
           String proxyHost = proxyInfo.getHost();
           String proxyPort = String.valueOf(proxyInfo.getPort());
-          if(proxyHost != null && proxyPort != null)
-          {
+          if (proxyHost != null && proxyPort != null) {
             result.success("PROXY " + proxyHost + ":" + proxyPort);
           }
         }
