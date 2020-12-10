8bb78d7f9c30d75805e4b1bdcf3cac3607bd6a33
Bruno Bowden <github@brunobowden.com>
2020-12-09 23:32:55 -0800

    DEVELOPMENT_ONLY deprecated (#1816)
    - DEVELOPMENT_ONLY environment variable deprecated
    - Replaced by build flavors
    - Remove "BUILT_AT" as it impedes reproducible build
    - iOS CI build addition: `./tools/gen-client-buildinfo.sh`
    - manual-build.md improvements

--- client/lib/pages/about_page.dart
+++ client/lib/pages/about_page.dart
@@ -47,8 +47,6 @@ class AboutPage extends StatelessWidget {
         'Debug Info:',
         'Pkg info: ${packageInfo?.appName}, ${packageInfo?.packageName}, ${packageInfo?.version}, ${packageInfo?.buildNumber}',
         'Git commit: ${BuildInfo.GIT_SHA}',
-        'Built at: ${BuildInfo.BUILT_AT}',
-        'Development build: ${BuildInfo.DEVELOPMENT_ONLY}',
         'Platform locale: ${Platform.localeName}',
         'Platform version: ${Platform.operatingSystem} ${Platform.operatingSystemVersion}',
         'Flutter locale: ${Localizations.localeOf(bc).toString()}',
