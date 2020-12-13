76e8c488bf6571e9a6830f26368a17aa60c70610
matthewblain <mb7oo@hotmail.com>
2020-12-12 21:39:41 -0800

    Re-build with flutter proto plugin 19.2.0+1

--- client/lib/proto/api/who/who.pbjson.dart
+++ client/lib/proto/api/who/who.pbjson.dart
@@ -3,7 +3,7 @@
 //  source: api/who/who.proto
 //
 // @dart = 2.3
-// ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
 
 const JurisdictionType$json = const {
   '1': 'JurisdictionType',
