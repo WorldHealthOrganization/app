c7c17c9d966f130bc638006f674f0aa7c1e66310
Tushar Nikam <champ96k@gmail.com>
2021-01-01 04:24:05 +0530

    Bugfix on Home Screen shows a blank animation for countries having 0 daily National   (#1906)
    * bug fix #1860 Home Screen shows a blank animation for countries having 0 daily National Cases
    
    * code refactor
    
    Co-authored-by: Tushar Nikam <tushar.nikam@hashtagpoker.com>

--- client/lib/components/home_page_sections/home_page_recent_numbers.dart
+++ client/lib/components/home_page_sections/home_page_recent_numbers.dart
@@ -104,12 +104,12 @@ class __HomeStatsFaderState extends State<_HomeStatsFader>
           firstChild: Observer(
             builder: (_) => _HomeStatCard(
               stat: widget.statsStore.countryStats != null &&
-                      widget.statsStore.countryDailyCases > 0
+                      widget.statsStore.countryDailyCases >= 0
                   ? widget.numFmt.format(widget.statsStore.countryDailyCases)
                   : '',
               // TODO: localize
               title: widget.statsStore.countryStats != null &&
-                      widget.statsStore.countryDailyCases > 0
+                      widget.statsStore.countryDailyCases >= 0
                   ? 'National Cases'
                   : '',
             ),
