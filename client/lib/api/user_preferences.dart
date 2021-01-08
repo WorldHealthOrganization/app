04b8a4f7b67a377d508b1a822e76705b4363e7b1
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-01-08 12:27:54 -0800

    set performance monitering on and off based on analyticssetting

--- client/lib/api/user_preferences.dart
+++ client/lib/api/user_preferences.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 import 'package:uuid/uuid_util.dart';
@@ -101,10 +102,12 @@ class UserPreferences {
 
   Future<bool> setAnalyticsEnabled(bool value) async {
     var analytics = FirebaseAnalytics();
+    var performance = FirebasePerformance.instance;
     if (!value) {
       await analytics.resetAnalyticsData();
     }
     await analytics.setAnalyticsCollectionEnabled(value);
+    await performance.setPerformanceCollectionEnabled(value);
     var result = (await SharedPreferences.getInstance())
         .setBool(UserPreferenceKey.AnalyticsEnabled.toString(), value);
     await _updateFirebaseAutoInit();
