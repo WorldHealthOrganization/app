3c2ee41ad1a3fb31a63425e9ee2968f86c0d90e2
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-07 10:21:27 -0800

    finally done

--- client/lib/api/who_date_format.dart
+++ client/lib/api/who_date_format.dart
@@ -3,11 +3,7 @@ import 'package:intl/intl.dart';
 extension WHOFormat on DateTime {
   String get whoFormat => DateFormat.yMMMd().format(
         // Timestamps are for midnight so the day it applies to is ambiguous.
-        // Add 1 day to match the WHO Dashboard: http://covid19.who.int
-        add(
-          Duration(
-            days: 1,
-          ),
-        ),
+        // Sets date to UTC to match the WHO Dashboard: http://covid19.who.int
+        toUtc()
       );
 }
