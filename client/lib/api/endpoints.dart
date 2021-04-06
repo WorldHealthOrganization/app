3ec833ecd4425859b22d86666f7aeeee66b467f2
Bezhou Feng <bezfeng@gmail.com>
2021-04-05 21:13:21 -0400

    Address review comments
    * Remove old prod code
    * Add TODOs

--- client/lib/api/endpoints.dart
+++ client/lib/api/endpoints.dart
@@ -3,14 +3,9 @@
 class Endpoint {
   static const _whoMhPrefix = 'who-mh-';
   static const _whoMh2Prefix = 'who-mh2-';
-  static const _prodProjectId = 'who-mh-prod';
-  static const _prodServiceUrl = 'https://covid19app.who.int';
 
+  // TODO: Figure out a way to avoid hardcoding regions in client code.
   static const _region = 'europe-west6';
-
-  static bool _isProd(String projectId) {
-    return projectId == _prodProjectId;
-  }
 
   static String _projectIdShort(String projectId) {
     if (!projectId.startsWith(_whoMh2Prefix) &&
@@ -18,32 +13,27 @@ class Endpoint {
       throw Exception(
           "Project: $projectId doesn't match prefix: $_whoMhPrefix");
     }
-    return _isProd(projectId)
-        ? projectId.substring(_whoMhPrefix.length)
-        : projectId.substring(_whoMh2Prefix.length);
+    return projectId.startsWith(_whoMh2Prefix)
+        ? projectId.substring(_whoMh2Prefix.length)
+        : projectId.substring(_whoMhPrefix.length);
   }
 
   static String _serviceUrl(String projectId) {
-    if (_isProd(projectId)) {
-      return '${_prodServiceUrl}/WhoService';
-    }
     return 'https://${_region}-${projectId}.cloudfunctions.net';
   }
 
   static String _staticContentUrl(String projectId) {
-    if (_isProd(projectId)) {
-      return _prodServiceUrl;
-    }
     return 'https://${projectId}.web.app';
   }
 
+  // TODO: Hook up prod endpoints once V2 is deployed in prod.
   final bool isProd;
   final String projectIdShort;
   final String serviceUrl;
   final String staticContentUrl;
 
   Endpoint(String projectId)
-      : isProd = _isProd(projectId),
+      : isProd = false,
         projectIdShort = _projectIdShort(projectId),
         serviceUrl = _serviceUrl(projectId),
         staticContentUrl = _staticContentUrl(projectId);
